50a9b61f6c7e8e28ff815d4fc142bb38c7acc187
Bryan Miller <bmilcs@yahoo.com>
2023-07-16 17:01:50 -0400

    Add sass package & configuration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,4 +1,5 @@
 import react from "@vitejs/plugin-react-swc";
+import sass from "sass";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
@@ -6,5 +7,12 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: { "@": "/src" },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        implementation: sass,
+      },
+    },
   },
 });
