24d5267279696cd78b8a2381254704dd487428ad
Bryan Miller <bmilcs@yahoo.com>
2023-07-22 16:37:59 -0400

    Add field requirements, password hashing & compare methods to UserSchema

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,9 +1,10 @@
+import bcrypt from "bcryptjs";
 import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 const ObjectId = Schema.Types.ObjectId;
 
-export type UserDocument = mongoose.Document & {
+export type TUserDocument = mongoose.Document & {
   firstName: string;
   lastName: string;
   email: string;
@@ -17,17 +18,77 @@ export type UserDocument = mongoose.Document & {
 };
 
 const UserSchema = new Schema({
-  firstName: String,
-  lastName: String,
-  email: String,
-  password: String,
-  address: String,
-  city: String,
-  state: String,
-  zip: String,
-  phone: String,
-  events: [{ type: ObjectId, ref: "Event" }],
+  firstName: {
+    type: String,
+    required: [true, "First name is required."],
+    length: { min: 1, max: 50 },
+  },
+  lastName: {
+    type: String,
+    required: [true, "Last name is required."],
+    length: { min: 1, max: 50 },
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Email is required."],
+    length: { min: 1, max: 320 },
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required."],
+    length: { min: 8, max: 50 },
+  },
+  address: {
+    type: String,
+    required: [true, "Address is required."],
+    length: { min: 1, max: 200 },
+  },
+  city: {
+    type: String,
+    required: [true, "City is required."],
+    length: { min: 1, max: 100 },
+  },
+  state: {
+    type: String,
+    required: [true, "State is required."],
+    length: { min: 2, max: 2 },
+  },
+  zip: {
+    type: String,
+    required: [true, "Zip code is required."],
+    length: { min: 5, max: 5 },
+  },
+  phone: { type: String, required: [true, "Phone is required."], length: 10 },
+  events: [{ type: ObjectId, ref: "Event", required: false }],
 });
+
+// hash password before saving to database
+UserSchema.pre<TUserDocument>("save", async function (next) {
+  const user = this;
+  if (!user.isModified("password")) {
+    return next();
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+    user.password = hashedPassword;
+    next();
+  } catch (err: any) {
+    return next(err);
+  }
+});
+
+// compare supplied password vs. stored password
+UserSchema.methods.comparePassword = function (
+  candidatePassword: string,
+  next: (err: any, isMatch: boolean) => void,
+) {
+  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+    if (err) return next(err, false);
+    next(null, isMatch);
+  });
+};
 
 const UserModel = mongoose.model("User", UserSchema);
 
