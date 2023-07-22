ce38e05936974703326c195e0cac2568e1c6358c
Bryan Miller <bmilcs@yahoo.com>
2023-07-22 14:02:53 -0400

    Add interfaces for vite's env var handling: ImportMeta

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -2,3 +2,11 @@
 
 declare module "*.module.css";
 declare module "*.module.scss";
+
+interface ImportMetaEnv {
+  VITE_API_ENDPOINT: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
