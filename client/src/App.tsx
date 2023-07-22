3f06efbd3a1783ea7917f90fc9205de9611ae12b
Bryan Miller <bmilcs@yahoo.com>
2023-07-22 14:03:19 -0400

    Add /signup route to client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { Contact } from "@/components/pages/Contact/Contact";
 import { Gallery } from "@/components/pages/Gallery/Gallery";
 import { Home } from "@/components/pages/Home/Home";
 import { Services } from "@/components/pages/Services/Services";
+import { Signup } from "@/components/pages/Signup/Signup";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path="/services" element={<Services />} />
           <Route path="/gallery" element={<Gallery />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </Layout>
     </BrowserRouter>
