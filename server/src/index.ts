00015cc328c9296fcdc1d502594ec8d4971e2ec2
Bryan Miller <bmilcs@yahoo.com>
2023-07-23 15:12:15 -0400

    Refactor server index: cookieParser, rm passport, fix cors & logout route

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,9 @@
 import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
-import passport from "passport";
 import * as eventController from "./controllers/eventController";
 import * as userController from "./controllers/userController";
 
@@ -17,6 +17,8 @@ const corsOrigin =
   process.env.NODE_ENV === "production"
     ? process.env.PRODUCTION_URL
     : `http://localhost:3001`;
+export const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
+export const refreshTokenSecret = process.env.REFRESH_TOKEN_SECRET;
 
 //
 // express app
@@ -31,14 +33,17 @@ const app = express();
 const corsOptions = {
   origin: corsOrigin,
   optionsSuccessStatus: 200,
+  credentials: true, // allow cookies to be sent from the client to the server
 };
 app.use(cors(corsOptions));
 
 //
-// body parser middleware: parse incoming data
+// body parser middleware
 //
 
+// parse incoming data to req.body
 app.use(bodyParser.json());
+// parse application/x-www-form-urlencoded: easier testing with Postman or plain HTML forms
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //
@@ -59,8 +64,8 @@ async function connectDB() {
 // authentication
 //
 
-// passport middleware
-app.use(passport.initialize());
+// cookie parser middleware: used for jwt tokens
+app.use(cookieParser());
 
 //
 // routes
@@ -71,7 +76,7 @@ app.get("/api/user", userController.userGet);
 app.post("/api/user", userController.userPost);
 app.post("/api/user/login", userController.userLogin);
 app.post("/api/user/signup", userController.userSignup);
-app.post("/api/user/logout", userController.userLogout);
+app.get("/api/user/logout", userController.userLogout);
 
 // event routes
 app.get("/api/event/:id", eventController.eventGet);
