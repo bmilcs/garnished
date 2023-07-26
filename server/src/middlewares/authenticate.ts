2b417dfe74d08045dca0564c80261ab77eaf1847
Bryan Miller <bmilcs@yahoo.com>
2023-07-26 16:18:38 -0400

    Refactor authenticate middleware: auto refresh access token when possible

--- server/src/middlewares/authenticate.ts
+++ server/src/middlewares/authenticate.ts
@@ -1,8 +1,9 @@
+import { accessTokenSecret, refreshTokenSecret } from "@/index";
+import { setJwtAccessTokenCookie } from "@/middlewares/setJwtCookies";
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
-import { accessTokenSecret } from "../index";
 
-export type TJWTPayload = {
+export type TJwtPayload = {
   userId: string;
 };
 
@@ -10,28 +11,48 @@ export interface IAuthRequest extends Request {
   userId?: string;
 }
 
-// middleware to check authentication status
-// adds .userId to the request object
+//
+// authenticate middleware: check authentication status based on jwt cookies
+// - adds user details to request object
+// - refreshes access token if expired
+//
+
 const authenticate = (req: IAuthRequest, res: Response, next: NextFunction) => {
-  const accessToken = req.cookies.access_token;
-
-  // no accessToken provided, block access
-  if (!accessToken) {
-    return res.status(401).json({ msg: "Unauthorized." });
-  }
-
-  // missing accessTokenSecret on backend
-  if (!accessTokenSecret) {
+  // token secrets missing on backend: block access
+  if (!accessTokenSecret || !refreshTokenSecret) {
     return res.status(500).json({ msg: "Internal server error." });
   }
 
-  // add userId to the request object if the accessToken is valid
+  const { accessToken, refreshToken } = req.cookies;
+
+  // request is missing tokens: user is not authenticated
+  if (!accessToken || !refreshToken) {
+    return res.status(401).json({ msg: "Unauthorized." });
+  }
+
+  // decode access token
   try {
-    const decoded = jwt.verify(accessToken, accessTokenSecret) as TJWTPayload;
+    const decoded = jwt.verify(accessToken, accessTokenSecret) as TJwtPayload;
+
+    // accessToken is valid & hasn't expired
     req.userId = decoded.userId;
     return next();
-  } catch (error) {
-    return res.status(401).json({ msg: "Unauthorized." });
+  } catch {
+    // access token is expired / invalid: attempt to refresh access token
+    try {
+      const decoded = jwt.verify(
+        refreshToken,
+        refreshTokenSecret,
+      ) as TJwtPayload;
+
+      // refreshToken is valid: generate new accessToken
+      setJwtAccessTokenCookie(res, { userId: decoded.userId });
+      req.userId = decoded.userId;
+      return next();
+    } catch {
+      // accessToken & refreshToken are invalid / expired
+      return res.status(401).json({ msg: "Unauthorized." });
+    }
   }
 };
 
