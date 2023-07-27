72e693dfd7724b07f1bffbaa1ac6dd69cb83166a
Bryan Miller <bmilcs@yahoo.com>
2023-07-27 18:14:57 -0400

    Fix hamburger svg attributes (React error)

--- client/src/components/layout/Header/Header.tsx
+++ client/src/components/layout/Header/Header.tsx
@@ -20,14 +20,7 @@ export const Header: FC = () => {
           viewBox="0 0 24 24"
           className={styles.hamburger}
         >
-          <path
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M5 17h14M5 12h14M5 7h14"
-          />
+          <path fill="none" stroke="currentColor" d="M5 17h14M5 12h14M5 7h14" />
         </svg>
       </Button>
 
