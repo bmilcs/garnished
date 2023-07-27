74d1384006bb74ace08c08c084e6a3bbe695c444
Bryan Miller <bmilcs@yahoo.com>
2023-07-27 17:51:47 -0400

    Add constraints to eventSchema values on server

--- server/src/models/event.ts
+++ server/src/models/event.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 const ObjectId = Schema.Types.ObjectId;
 
-export type EventDocument = mongoose.Document & {
+export type TEventDocument = mongoose.Document & {
   user: string;
   date: Date;
   time: string;
@@ -11,7 +11,7 @@ export type EventDocument = mongoose.Document & {
   address: string;
   city: string;
   state: string;
-  zip: string;
+  zip: number;
   guests: number;
   hours: number;
   eventType: string;
@@ -29,26 +29,34 @@ export type EventDocument = mongoose.Document & {
 
 const EventSchema = new Schema({
   user: [{ type: ObjectId, ref: "User" }],
-  date: Date,
-  time: String,
-  locationDescription: String,
-  address: String,
-  city: String,
-  state: String,
-  zip: String,
-  guests: Number,
-  hours: Number,
-  eventType: String,
-  needBar: Boolean,
-  needTent: Boolean,
-  needAlcohol: Boolean,
-  needRunningWater: Boolean,
-  needRefrigeration: Boolean,
-  needDrinkware: Boolean,
-  beer: Boolean,
-  wine: Boolean,
-  specialtyDrinks: Boolean,
-  liquorPreferences: String,
+  date: { type: Date, required: true, isLength: { min: 10, max: 10 } },
+  time: { type: String, required: true, isLength: { min: 5, max: 5 } },
+  locationDescription: {
+    type: String,
+    required: true,
+    isLength: { min: 10, max: 250 },
+  },
+  address: { type: String, required: true, isLength: { min: 1, max: 200 } },
+  city: { type: String, required: true, isLength: { min: 1, max: 100 } },
+  state: { type: String, required: true, isLength: { min: 2, max: 2 } },
+  zip: { type: Number, required: true, isLength: { min: 5, max: 5 } },
+  guests: { type: Number, required: true, isLength: { min: 1, max: 10 } },
+  hours: { type: Number, required: true, isLength: { min: 1, max: 2 } },
+  eventType: { type: String, required: true, isLength: { min: 1, max: 200 } },
+  needBar: { type: Boolean, required: true },
+  needTent: { type: Boolean, required: true },
+  needAlcohol: { type: Boolean, required: true },
+  needRunningWater: { type: Boolean, required: true },
+  needRefrigeration: { type: Boolean, required: true },
+  needDrinkware: { type: Boolean, required: true },
+  beer: { type: Boolean, required: true },
+  wine: { type: Boolean, required: true },
+  specialtyDrinks: { type: Boolean, required: true },
+  liquorPreferences: {
+    type: String,
+    required: true,
+    isLength: { min: 1, max: 300 },
+  },
 });
 
 const EventModel = mongoose.model("Event", EventSchema);
