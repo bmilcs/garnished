adee7d19b806f04015eaee0fa01bfeed2dc42e66
Bryan Miller <bmilcs@yahoo.com>
2023-07-27 17:56:15 -0400

    Add event type to client

---
+++ client/src/types/eventTypes.ts
@@ -0,0 +1,24 @@
+export type TEvent = {
+  _id: string;
+  date: string;
+  time: string;
+  locationDescription: string;
+  address: string;
+  city: string;
+  state: string;
+  // leading zeros cause a TS error
+  zip: string;
+  guests: number;
+  hours: number;
+  eventType: string;
+  needBar: boolean;
+  needTent: boolean;
+  needAlcohol: boolean;
+  needRunningWater: boolean;
+  needRefrigeration: boolean;
+  needDrinkware: boolean;
+  beer: boolean;
+  wine: boolean;
+  specialtyDrinks: boolean;
+  liquorPreferences: string;
+};
