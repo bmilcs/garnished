2d8ddf93f32def07683d20ee4f22b3e777005b87
Bryan Miller <bmilcs@yahoo.com>
2023-08-06 15:33:24 -0400

    Add footer content: logo, nav, socials & credits to client

--- client/src/components/layout/Footer/Footer.tsx
+++ client/src/components/layout/Footer/Footer.tsx
@@ -1,6 +1,83 @@
+import { Facebook } from "@/components/common/Facebook/Facebook";
+import { GitHub } from "@/components/common/GitHub/GitHub";
+import { Instagram } from "@/components/common/Instagram/Instagram";
+import ScrollAnimator from "@/components/common/ScrollAnimator/ScrollAnimator";
 import { FC } from "react";
+import { Link } from "react-router-dom";
 import styles from "./Footer.module.scss";
 
 export const Footer: FC = () => {
-  return <footer className={styles.footer}>Footer</footer>;
+  return (
+    <footer className={styles.footer}>
+      <div className={`column ${styles.footerWrapper}`}>
+        <ScrollAnimator type="SLIDE_UP">
+          <h3 className={styles.footerLogo}>Garnished</h3>
+          <p>Premium Mobile Bar</p>
+        </ScrollAnimator>
+
+        <ScrollAnimator type="SLIDE_UP" delay={0.2}>
+          <nav className={styles.footerNav}>
+            <ul className={styles.footerNavUl}>
+              <li>
+                <Link className={styles.footerLink} to="/">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link className={styles.footerLink} to="/services">
+                  Services
+                </Link>
+              </li>
+              <li>
+                <Link className={styles.footerLink} to="/gallery">
+                  Gallery
+                </Link>
+              </li>
+              <li>
+                <Link className={styles.footerLink} to="/contact">
+                  Contact
+                </Link>
+              </li>
+              <li>
+                <Link className={styles.footerLink} to="/login">
+                  Login
+                </Link>
+              </li>
+              <li>
+                <Link className={styles.footerLink} to="/signup">
+                  Sign Up
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </ScrollAnimator>
+
+        <ScrollAnimator type="SLIDE_UP" delay={0.4}>
+          <div className={styles.social}>
+            <Facebook />
+            <Instagram />
+          </div>
+        </ScrollAnimator>
+      </div>
+
+      <ScrollAnimator type="FADE_IN" className={styles.credits} delay={0.6}>
+        <div className={`column ${styles.creditsWrapper}`}>
+          <p className={styles.footerCopy}>&copy; 2023 Garnished LLP</p>
+
+          <div className={styles.github}>
+            <p>
+              Created by{" "}
+              <a href="https://bmilcs.com" target="_blank">
+                Bryan Miller
+              </a>
+            </p>
+            <GitHub
+              url="https://github.com/bmilcs/garnished"
+              className={styles.githubIcon}
+            />
+          </div>
+        </div>
+      </ScrollAnimator>
+    </footer>
+  );
 };
