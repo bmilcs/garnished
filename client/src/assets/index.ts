6f6b492c1c18f1f50ee21a13e3eba11b9d12344e
Bryan Miller <bmilcs@yahoo.com>
2023-08-10 16:36:33 -0400

    Add asset index file with image object export

---
+++ client/src/assets/index.ts
@@ -0,0 +1,57 @@
+import specialtyDrink01 from "./specialtyDrink-01.jpg";
+import specialtyDrink02 from "./specialtyDrink-02.jpg";
+import specialtyDrink03 from "./specialtyDrink-03.jpg";
+import specialtyDrink04 from "./specialtyDrink-04.jpg";
+import specialtyDrink05 from "./specialtyDrink-05.jpg";
+import specialtyDrink06 from "./specialtyDrink-06.jpg";
+import specialtyDrink07 from "./specialtyDrink-07.jpg";
+import specialtyDrink08 from "./specialtyDrink-08.jpg";
+import specialtyDrink09 from "./specialtyDrink-09.jpg";
+import specialtyDrink10 from "./specialtyDrink-10.jpg";
+import specialtyDrink11 from "./specialtyDrink-11.jpg";
+import specialtyDrink12 from "./specialtyDrink-12.jpg";
+import specialtyDrink13 from "./specialtyDrink-13.jpg";
+import specialtyDrink14 from "./specialtyDrink-14.jpg";
+import specialtyDrink15 from "./specialtyDrink-15.jpg";
+import specialtyDrink16 from "./specialtyDrink-16.jpg";
+import specialtyDrink17 from "./specialtyDrink-17.jpg";
+import specialtyDrink18 from "./specialtyDrink-18.jpg";
+import specialtyDrink19 from "./specialtyDrink-19.jpg";
+import specialtyDrink20 from "./specialtyDrink-20.jpg";
+import specialtyDrink21 from "./specialtyDrink-21.jpg";
+import specialtyDrink22 from "./specialtyDrink-22.jpg";
+import specialtyDrink23 from "./specialtyDrink-23.jpg";
+import specialtyDrink24 from "./specialtyDrink-24.jpg";
+import specialtyDrink25 from "./specialtyDrink-25.jpg";
+import specialtyDrink26 from "./specialtyDrink-26.jpg";
+import specialtyDrink27 from "./specialtyDrink-27.jpg";
+
+export {
+  specialtyDrink01,
+  specialtyDrink02,
+  specialtyDrink03,
+  specialtyDrink04,
+  specialtyDrink05,
+  specialtyDrink06,
+  specialtyDrink07,
+  specialtyDrink08,
+  specialtyDrink09,
+  specialtyDrink10,
+  specialtyDrink11,
+  specialtyDrink12,
+  specialtyDrink13,
+  specialtyDrink14,
+  specialtyDrink15,
+  specialtyDrink16,
+  specialtyDrink17,
+  specialtyDrink18,
+  specialtyDrink19,
+  specialtyDrink20,
+  specialtyDrink21,
+  specialtyDrink22,
+  specialtyDrink23,
+  specialtyDrink24,
+  specialtyDrink25,
+  specialtyDrink26,
+  specialtyDrink27,
+};
