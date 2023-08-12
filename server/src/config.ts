8d3e1ff47967e04adcf8db0d42be55651f3525d2
Bryan Miller <bmilcs@yahoo.com>
2023-08-12 15:06:32 -0400

    Add email from & owners emails to config & env example on server

--- server/src/config.ts
+++ server/src/config.ts
@@ -15,9 +15,15 @@ export const MONGO_DB = process.env.MONGODB || "";
 export const PRODUCTION_URL = process.env.PRODUCTION_URL || "";
 
 // jwt token secrets
-export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
-export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
+export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || "";
+export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET || "";
 
 // nodemailer email credentials
-export const EMAIL_USERNAME = process.env.EMAIL_USERNAME;
-export const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD;
+export const EMAIL_USERNAME = process.env.EMAIL_USERNAME || "";
+export const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD || "";
+
+// nodemailer from email address
+export const EMAIL_FROM = process.env.EMAIL_FROM || "";
+
+// business owners email addresses
+export const OWNERS_EMAILS = process.env.OWNERS_EMAILS || "";
