a78af9040c78749a3674fb8ca1b411955b6c67e0
Bryan Miller <bmilcs@yahoo.com>
2023-08-12 13:42:26 -0400

    Add config.ts module to import .env variables on server

---
+++ server/src/config.ts
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// node environment
+export const NODE_ENV = process.env.NODE_ENV || "development";
+
+// app port
+export const PORT = process.env.PORT || 3000;
+
+// database connection string: mongodb atlas
+export const MONGO_DB = process.env.MONGODB || "";
+
+// production url: required for CORS
+export const PRODUCTION_URL = process.env.PRODUCTION_URL || "";
+
+// jwt token secrets
+export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
+
+// nodemailer email credentials
+export const EMAIL_USERNAME = process.env.EMAIL_USERNAME;
+export const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD;
