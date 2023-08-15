dcef6689071a55505cea5563276ff2972b5bc933
Bryan Miller <bmilcs@yahoo.com>
2023-08-15 16:32:33 -0400

    Add formatter utility module to client

---
+++ client/src/utils/formatters.ts
@@ -0,0 +1,19 @@
+export const formatPhoneNumber = (phoneNumber: number) => {
+  const phoneNumberString = phoneNumber.toString();
+  const areaCode = phoneNumberString.slice(0, 3);
+  const firstThree = phoneNumberString.slice(3, 6);
+  const lastFour = phoneNumberString.slice(6, 10);
+  return `(${areaCode}) ${firstThree}-${lastFour}`;
+};
+
+export const formatDate = (date: string) => {
+  return new Date(date).toLocaleDateString();
+};
+
+export const formatTime = (time: string) => {
+  const timeArr = time.split(":");
+  const hours = +timeArr[0] > 12 ? +timeArr[0] - 12 : timeArr[0];
+  const minutes = timeArr[1];
+  const suffix = +timeArr[0] > 12 ? "PM" : "AM";
+  return `${hours}:${minutes} ${suffix}`;
+};
