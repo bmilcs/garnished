61baf627aa4656fb930a9384b3fa0ea3f99e3cb1
Bryan Miller <bmilcs@yahoo.com>
2023-08-15 16:34:32 -0400

    Add all details & delete functionality to event page on client

--- client/src/components/pages/Event/Event.tsx
+++ client/src/components/pages/Event/Event.tsx
@@ -1,80 +1,141 @@
+import { Button } from "@/components/common/Button/Button";
+import ScrollAnimator from "@/components/common/ScrollAnimator/ScrollAnimator";
 import { AuthContext } from "@/hooks/useAuthContext";
-import { TEvent } from "@/types/eventTypes";
+import { TEventWithId } from "@/types/eventTypes";
 import { getApiEndpoint } from "@/utils/apiConfig";
+import { formatDate, formatTime } from "@/utils/formatters";
 import { FC, useContext, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import styles from "./Event.module.scss";
 
 type TEventApiResponse = {
   msg: string;
-  event: TEvent;
+  event: TEventWithId;
 };
 
 export const Event: FC = () => {
   const navigate = useNavigate();
   const { id: eventId } = useParams();
   const { isLoggedIn } = useContext(AuthContext);
-  const [eventData, setEventData] = useState<TEvent | null>(null);
+  const [eventData, setEventData] = useState<TEventWithId | null>(null);
   const [error, setError] = useState("");
 
-  // retrieve user data on initial page load
-  useEffect(() => {
-    if (!isLoggedIn) {
-      navigate("/login");
-      return;
-    }
-
-    const getUserData = async () => {
-      const apiBasePath = getApiEndpoint();
-
-      if (!eventId) {
-        console.log("No event id provided");
+  useEffect(
+    function fetchUserDataOnInitialPageLoad() {
+      if (!isLoggedIn) {
+        navigate("/login");
         return;
       }
 
-      const url = `${apiBasePath}/event/${eventId}`;
-      try {
-        const res = await fetch(url, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          credentials: "include",
-        });
+      getUserData().catch((error: Error) => setError(error.message));
 
-        const data = (await res.json()) as TEventApiResponse;
+      async function getUserData() {
+        if (!eventId) return;
 
-        console.log(data);
+        const apiBasePath = getApiEndpoint();
+        const url = `${apiBasePath}/event/${eventId}`;
+        try {
+          const res = await fetch(url, {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            credentials: "include",
+          });
 
-        if (data.event) {
-          // user is logged in, set user data
-          setEventData(data.event);
+          const data = (await res.json()) as TEventApiResponse;
+          if (data.event) {
+            // user is logged in, set user data
+            setEventData(data.event);
+          }
+        } catch (e) {
+          console.log(e);
+          throw new Error(
+            "Something went wrong while retrieving your event. Try again later.",
+          );
         }
-      } catch (e) {
-        console.log(e);
-        throw new Error(
-          "Something went wrong while retrieving your event. Try again later.",
-        );
       }
-    };
+    },
+    [isLoggedIn, navigate, eventId],
+  );
 
-    getUserData().catch((error: Error) => setError(error.message));
-  }, [isLoggedIn, navigate, eventId]);
+  const handleDelete = async () => {
+    if (!eventId) return;
+
+    const apiBasePath = getApiEndpoint();
+    const url = `${apiBasePath}/event/${eventId}`;
+    try {
+      const res = await fetch(url, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      });
+
+      if (res.status === 200) {
+        navigate("/user");
+      }
+    } catch (e) {
+      console.log(e);
+      throw new Error(
+        "Something went wrong while retrieving your event. Try again later.",
+      );
+    }
+  };
 
   return (
-    <section className={`column content-spacer ${styles.signup}`}>
-      <h2>Event Details</h2>
+    <section className="content-spacer user-section">
+      <ScrollAnimator type="SLIDE_DOWN" className="column user-section-wrapper">
+        <h2>Your Event</h2>
 
-      {eventData && (
-        <div>
-          <p>{eventData.address}</p>
-          <p>{eventData.city}</p>
-          <p>{eventData.zip}</p>
-          <p>{eventData.state}</p>
-        </div>
-      )}
+        {eventData && (
+          <ScrollAnimator type="SLIDE_DOWN" className="user-section-card">
+            <h4>Details</h4>
 
-      {error && <p className="error">{error}</p>}
+            <p>{eventData.eventType}</p>
+            <p>
+              {formatTime(eventData.time)} on {formatDate(eventData.date)}
+            </p>
+            <p>Guests: {eventData.guests}</p>
+            <p>Hours: {eventData.hours}</p>
+
+            <h4>Location</h4>
+
+            <p>Located: {eventData.locationDescription}</p>
+            <p>{eventData.address}</p>
+            <p>
+              {eventData.city}, {eventData.state} {eventData.zip}
+            </p>
+
+            <h4>Services</h4>
+
+            {eventData.needBar && <p>Mobile Bar</p>}
+            {eventData.needTent && <p>Tent</p>}
+            {eventData.needAlcohol && <p>Alcohol</p>}
+            {eventData.needRunningWater && <p>Running Water</p>}
+            {eventData.needRefrigeration && <p>Refrigeration</p>}
+            {eventData.needDrinkware && <p>Drinkware</p>}
+            {eventData.beer && <p>Beer</p>}
+            {eventData.wine && <p>Wine</p>}
+            {eventData.specialtyDrinks && <p>Specialty Drinks</p>}
+
+            <h4>Liquor Preferences</h4>
+
+            {eventData.liquorPreferences}
+          </ScrollAnimator>
+        )}
+
+        <ScrollAnimator type="SLIDE_DOWN" className="button-wrapper">
+          <Button type="primary" onClick={handleDelete}>
+            Delete Event
+          </Button>
+          <Button type="primary" onClick={() => navigate("/user")}>
+            Back to Dashboard
+          </Button>
+        </ScrollAnimator>
+
+        {error && <p className="error">{error}</p>}
+      </ScrollAnimator>
     </section>
   );
 };
