d349abfd0ad33d6fe29c1de86ff856b4b59a5828
Bryan Miller <bmilcs@yahoo.com>
2023-08-17 16:33:35 -0400

    Add responsiveness to user dashboard page on client

--- client/src/components/pages/User/User.tsx
+++ client/src/components/pages/User/User.tsx
@@ -71,47 +71,51 @@ export const User: FC = () => {
   return (
     <section className={`content-spacer user-section`}>
       <div className={`column user-section-wrapper`}>
-        <div className={styles.dashboardHeader}>
+        <ScrollAnimator type="SLIDE_DOWN" className={styles.dashboardHeader}>
           <h2>User Dashboard</h2>
           <p>Welcome back, {userData?.firstName}!</p>
+        </ScrollAnimator>
+
+        <div className={styles.contentWrapper}>
+          {userData && (
+            <ScrollAnimator type="SLIDE_RIGHT" className="user-section-card">
+              <h4 className={styles.userDetailsHeader}>Personal Info</h4>
+              <p>
+                {userData.firstName} {userData.lastName}
+              </p>
+              <p>{userData.address}</p>
+              <p>
+                {userData.city}, {userData.state}, {userData.zip}
+              </p>
+
+              <h4 className={styles.userDetailsHeader}>Contact Info</h4>
+              <p>{formatPhoneNumber(userData.phone)}</p>
+              <p>{userData.username}</p>
+            </ScrollAnimator>
+          )}
+
+          {userData && userData.events.length > 0 && (
+            <ScrollAnimator
+              type="SLIDE_UP"
+              delay={0.2}
+              className="user-section-card"
+            >
+              <h4 className={styles.userDetailsHeader}>Your Events</h4>
+
+              {userData.events.map(event => (
+                <Link to={`/event/${event._id}`} key={event._id}>
+                  {formatDate(event.date)} - {event.eventType}
+                </Link>
+              ))}
+            </ScrollAnimator>
+          )}
         </div>
-
-        {userData && (
-          <ScrollAnimator type="SLIDE_RIGHT" className="user-section-card">
-            <h4 className={styles.userDetailsHeader}>Personal Info</h4>
-            <p>
-              {userData.firstName} {userData.lastName}
-            </p>
-            <p>{userData.address}</p>
-            <p>
-              {userData.city}, {userData.state}, {userData.zip}
-            </p>
-
-            <h4 className={styles.userDetailsHeader}>Contact Info</h4>
-            <p>{formatPhoneNumber(userData.phone)}</p>
-            <p>{userData.username}</p>
-          </ScrollAnimator>
-        )}
-
-        {userData && userData.events.length > 0 && (
-          <ScrollAnimator
-            type="SLIDE_UP"
-            delay={0.2}
-            className="user-section-card"
-          >
-            {userData.events.map(event => (
-              <Link to={`/event/${event._id}`} key={event._id}>
-                {formatDate(event.date)} - {event.eventType}
-              </Link>
-            ))}
-          </ScrollAnimator>
-        )}
 
         {error && <p className="error">{error}</p>}
 
         <ScrollAnimator type="SLIDE_UP" delay={0.4} className="button-wrapper">
-          <Button type="primary" link="/new-event">
-            New Event
+          <Button type="secondary" link="/new-event">
+            Create New Event
           </Button>
 
           <Button type="primary" onClick={handleLogout}>
