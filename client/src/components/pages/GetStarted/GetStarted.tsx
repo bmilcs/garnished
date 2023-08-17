402bcf21b78e9335651b554b6d8f17843711ff63
Bryan Miller <bmilcs@yahoo.com>
2023-08-17 15:56:47 -0400

    Add content & style get started page on client

--- client/src/components/pages/GetStarted/GetStarted.tsx
+++ client/src/components/pages/GetStarted/GetStarted.tsx
@@ -1,24 +1,55 @@
+import { specialtyDrink08 as backgroundImage } from "@/assets";
 import { Button } from "@/components/common/Button/Button";
+import { Hero } from "@/components/common/Hero/Hero";
+import { LoginForm } from "@/components/common/LoginForm/LoginForm";
+import ScrollAnimator from "@/components/common/ScrollAnimator/ScrollAnimator";
+import styles from "./GetStarted.module.scss";
 
 export const GetStarted = () => {
   return (
     <>
-      <section className="column">
-        <h2>Get Started</h2>
-        <p>Ready to bring your event to the next level?</p>
-        <p>
-          Our process is simple. Create an account & fill out the details of
-          your event. We will review it, create an estimate and we'll go from
-          there.
-        </p>
+      <Hero
+        backgroundImage={backgroundImage}
+        title="Get"
+        titleSpan="Started"
+        subtitle="Ready to bring your event to the next level?"
+        heightInVH={35}
+      />
 
-        <Button type="primary" link="/signup">
-          Sign Up
-        </Button>
+      <section className={`content-spacer ${styles.getStarted}`}>
+        <div className={`column content-spacer ${styles.getStartedWrapper}`}>
+          <ScrollAnimator type="SLIDE_UP">
+            <h2 className={styles.heading}>Here's How It Works</h2>
+            <p>Our process is simple:</p>
 
-        <Button type="primary" link="/login">
-          Login
-        </Button>
+            <ol className={styles.instructions}>
+              <li>Signup for an account</li>
+              <li>Fill out a new event form</li>
+            </ol>
+
+            <p>
+              From there, Garnished's customer service team will review the
+              details, craft a personalized estimate and connect with you to
+              finalize the details.
+            </p>
+
+            <ScrollAnimator
+              type="SLIDE_UP"
+              delay={0.5}
+              className={styles.signupButtonWrapper}
+            >
+              <Button
+                type="hero"
+                link="/signup"
+                className={styles.signupButton}
+              >
+                Sign Up Now
+              </Button>
+            </ScrollAnimator>
+          </ScrollAnimator>
+
+          <LoginForm className={styles.loginForm} />
+        </div>
       </section>
     </>
   );
