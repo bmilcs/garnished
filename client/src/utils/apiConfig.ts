4e0b5e350893f1cdc046499a3ba2c23739879f25
Bryan Miller <bmilcs@yahoo.com>
2023-08-18 18:37:03 -0400

    Add apiService: fetch wrapper function on client

--- client/src/utils/apiConfig.ts
+++
@@ -1,6 +0,0 @@
-// by default, "npm run dev" loads ".env.development"
-// and production loads ".env.production"
-
-export const getApiEndpoint = () => {
-  return import.meta.env.VITE_API_ENDPOINT;
-};
---
+++ client/src/utils/apiService.ts
@@ -0,0 +1,47 @@
+// temporary solution while converting to api service
+export const getApiEndpoint = () => import.meta.env.VITE_API_ENDPOINT;
+
+// by default, "npm run dev" loads from ".env.development"
+// and production loads ".env.production"
+const BASE_URL = import.meta.env.VITE_API_ENDPOINT;
+
+type HttpMethod = "GET" | "POST" | "PUT" | "DELETE";
+
+type TProps = {
+  path: string;
+  method: HttpMethod;
+  body?: Record<string, unknown>;
+  credentials?: boolean;
+};
+
+export const apiService = async <TApiResponse>({
+  method = "GET",
+  path,
+  body,
+  credentials = true,
+}: TProps) => {
+  const url = `${BASE_URL}/${path}`;
+
+  const options: RequestInit = {
+    method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  if (credentials) {
+    options.credentials = "include";
+  }
+
+  if (body) {
+    options.body = JSON.stringify(body);
+  }
+
+  try {
+    const res = await fetch(url, options);
+    const data = (await res.json()) as TApiResponse;
+    return { data, status: res.status };
+  } catch (error) {
+    throw new Error(String(error));
+  }
+};
