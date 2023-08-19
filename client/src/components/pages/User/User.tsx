aaff2d7827fee02c6930ff9315a961173379896c
Bryan Miller <bmilcs@yahoo.com>
2023-08-19 16:10:10 -0400

    Refactor user page to use useUserData hook on client

--- client/src/components/pages/User/User.tsx
+++ client/src/components/pages/User/User.tsx
@@ -1,128 +1,79 @@
 import { Button } from "@/components/common/Button/Button";
+import { HourglassSpinner } from "@/components/common/HourglassSpinner/HourglassSpinner";
 import ScrollAnimator from "@/components/common/ScrollAnimator/ScrollAnimator";
 import { AuthContext } from "@/hooks/useAuthContext";
-import { TUser } from "@/types/userTypes";
-import { getApiEndpoint } from "@/utils/apiConfig";
+import { useUserData } from "@/hooks/useUserData";
 import { formatDate, formatPhoneNumber } from "@/utils/formatters";
-import { FC, useContext, useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { FC, useContext } from "react";
+import { Link } from "react-router-dom";
 import styles from "./User.module.scss";
 
-type TUserApiResponse = {
-  msg: string;
-  user: TUser;
-};
-
 export const User: FC = () => {
-  const navigate = useNavigate();
-  const { isAuthPending, isLoggedIn, logout } = useContext(AuthContext);
-  const [userData, setUserData] = useState<TUser | null>(null);
-  const [error, setError] = useState("");
+  const { userData, isPending, error } = useUserData();
+  const { logout } = useContext(AuthContext);
 
-  // retrieve user data on initial page load
-  useEffect(() => {
-    if (isAuthPending) {
-      return;
-    }
+  if (isPending) return <HourglassSpinner />;
 
-    if (!isLoggedIn) {
-      navigate("/login");
-      return;
-    }
+  if (userData)
+    return (
+      <section className={`content-spacer user-section`}>
+        <div className={`column user-section-wrapper`}>
+          <ScrollAnimator type="SLIDE_DOWN" className={styles.dashboardHeader}>
+            <h2>User Dashboard</h2>
+            <p>Welcome back, {userData?.firstName}!</p>
+          </ScrollAnimator>
 
-    getUserData().catch((error: Error) => setError(error.message));
+          <div className={styles.contentWrapper}>
+            {userData && (
+              <ScrollAnimator type="SLIDE_RIGHT" className="user-section-card">
+                <h4 className={styles.userDetailsHeader}>Personal Info</h4>
+                <p>
+                  {userData.firstName} {userData.lastName}
+                </p>
+                <p>{userData.address}</p>
+                <p>
+                  {userData.city}, {userData.state}, {userData.zip}
+                </p>
 
-    async function getUserData() {
-      const apiBasePath = getApiEndpoint();
-      const url = `${apiBasePath}/user`;
-      try {
-        const res = await fetch(url, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          credentials: "include",
-        });
+                <h4 className={styles.userDetailsHeader}>Contact Info</h4>
+                <p>{formatPhoneNumber(userData.phone)}</p>
+                <p>{userData.username}</p>
+              </ScrollAnimator>
+            )}
 
-        const data = (await res.json()) as TUserApiResponse;
+            {userData && userData.events.length > 0 && (
+              <ScrollAnimator
+                type="SLIDE_UP"
+                delay={0.2}
+                className="user-section-card"
+              >
+                <h4 className={styles.userDetailsHeader}>Your Events</h4>
 
-        // if user is logged in
-        if (data.user) {
-          setUserData(data.user);
-        }
-      } catch (e) {
-        throw new Error(
-          "Something went wrong while logging in. Try again later.",
-        );
-      }
-    }
-  }, [isLoggedIn, isAuthPending, navigate]);
+                {userData.events.map(event => (
+                  <Link to={`/event/${event._id}`} key={event._id}>
+                    {formatDate(event.date)} - {event.eventType}
+                  </Link>
+                ))}
+              </ScrollAnimator>
+            )}
+          </div>
 
-  const handleLogout = () => {
-    logout()
-      .then(() => {
-        navigate("/login");
-      })
-      .catch(() =>
-        setError("Something went wrong while logging out. Try again later."),
-      );
-  };
+          {error && <p className="error">{error}</p>}
 
-  return (
-    <section className={`content-spacer user-section`}>
-      <div className={`column user-section-wrapper`}>
-        <ScrollAnimator type="SLIDE_DOWN" className={styles.dashboardHeader}>
-          <h2>User Dashboard</h2>
-          <p>Welcome back, {userData?.firstName}!</p>
-        </ScrollAnimator>
+          <ScrollAnimator
+            type="SLIDE_UP"
+            delay={0.4}
+            className="button-wrapper"
+          >
+            <Button type="secondary" link="/new-event">
+              Create New Event
+            </Button>
 
-        <div className={styles.contentWrapper}>
-          {userData && (
-            <ScrollAnimator type="SLIDE_RIGHT" className="user-section-card">
-              <h4 className={styles.userDetailsHeader}>Personal Info</h4>
-              <p>
-                {userData.firstName} {userData.lastName}
-              </p>
-              <p>{userData.address}</p>
-              <p>
-                {userData.city}, {userData.state}, {userData.zip}
-              </p>
-
-              <h4 className={styles.userDetailsHeader}>Contact Info</h4>
-              <p>{formatPhoneNumber(userData.phone)}</p>
-              <p>{userData.username}</p>
-            </ScrollAnimator>
-          )}
-
-          {userData && userData.events.length > 0 && (
-            <ScrollAnimator
-              type="SLIDE_UP"
-              delay={0.2}
-              className="user-section-card"
-            >
-              <h4 className={styles.userDetailsHeader}>Your Events</h4>
-
-              {userData.events.map(event => (
-                <Link to={`/event/${event._id}`} key={event._id}>
-                  {formatDate(event.date)} - {event.eventType}
-                </Link>
-              ))}
-            </ScrollAnimator>
-          )}
+            <Button type="primary" onClick={logout}>
+              Logout
+            </Button>
+          </ScrollAnimator>
         </div>
-
-        {error && <p className="error">{error}</p>}
-
-        <ScrollAnimator type="SLIDE_UP" delay={0.4} className="button-wrapper">
-          <Button type="secondary" link="/new-event">
-            Create New Event
-          </Button>
-
-          <Button type="primary" onClick={handleLogout}>
-            Logout
-          </Button>
-        </ScrollAnimator>
-      </div>
-    </section>
-  );
+      </section>
+    );
 };
