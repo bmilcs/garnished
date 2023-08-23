2f56c574b62b53ff16ec01ed366339a26bbd8bbc
Bryan Miller <bmilcs@yahoo.com>
2023-08-23 16:37:18 -0400

    Add tests/ setup module to server

---
+++ server/src/tests/setup.ts
@@ -0,0 +1,17 @@
+import { TEST_ACCESS_TOKEN, TEST_REFRESH_TOKEN } from "@/config";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import express from "express";
+import { test } from "mocha";
+import request from "supertest";
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+export const AUTH_COOKIES = [
+  `accessToken=${TEST_ACCESS_TOKEN}`,
+  `refreshToken=${TEST_REFRESH_TOKEN}`,
+];
+
+export { app, request, test };
