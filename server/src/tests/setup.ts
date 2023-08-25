e19ae10b8b451920db77ea921b9ca9d7e0c599e1
Bryan Miller <bmilcs@yahoo.com>
2023-08-25 15:35:22 -0400

    Fix user login test timeout error on server

--- server/src/tests/setup.ts
+++ server/src/tests/setup.ts
@@ -1,4 +1,6 @@
 import { TEST_ACCESS_TOKEN, TEST_REFRESH_TOKEN } from "@/config/env";
+import EventModel from "@/models/event";
+import UserModel from "@/models/user";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import express from "express";
@@ -34,7 +36,10 @@ const setupMongoTestServer = async () => {
   mongoServer = await MongoMemoryServer.create(mongodbMemoryServerOptions);
   const mongoUri = mongoServer.getUri();
   await mongoose.connect(mongoUri);
-  console.log(mongoose.connection.readyState);
+
+  // register schemas
+  mongoose.model("User", UserModel.schema);
+  mongoose.model("Event", EventModel.schema);
 
   mongoose.connection.on("error ", e => {
     if (e.message.code === "ETIMEDOUT") {
