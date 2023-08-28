984fc87ca212695cd8d8025a8332f144360056af
Bryan Miller <bmilcs@yahoo.com>
2023-08-28 12:52:32 -0400

    Reduce animation & swap svg out on home page

--- client/src/components/pages/Home/Home.tsx
+++ client/src/components/pages/Home/Home.tsx
@@ -1,3 +1,4 @@
+import { wine02 } from "@/assets";
 import drinkImage8 from "@/assets/specialtyDrink-08.jpg";
 import drinkImage7 from "@/assets/specialtyDrink-09.jpg";
 import drinkImage6 from "@/assets/specialtyDrink-10.jpg";
@@ -5,11 +6,9 @@ import drinkImage3 from "@/assets/specialtyDrink-11.jpg";
 import drinkImage4 from "@/assets/specialtyDrink-12.jpg";
 import drinkImage5 from "@/assets/specialtyDrink-13.jpg";
 import backgroundImage from "@/assets/specialtyDrink-21.jpg";
-import { BeerPourIcon } from "@/components/common/BeerPourIcon/BeerPourIcon";
 import { Button } from "@/components/common/Button/Button";
 import { CheckMarkIcon } from "@/components/common/CheckMarkIcon/CheckMarkIcon";
 import { Hero } from "@/components/common/Hero/Hero";
-import ScrollAnimator from "@/components/common/ScrollAnimator/ScrollAnimator";
 import { TiltDivider } from "@/components/common/TiltDivider/TiltDivider";
 import { FC } from "react";
 import styles from "./Home.module.scss";
@@ -91,21 +90,6 @@ const contentBlocks = [
       "Garnished invites you to embark on a journey of flavor, elegance, and celebration. Let us craft your story, where every detail, from drinks to garnishes, is meticulously designed to make you feel like an honored guest at your own gala.",
     image: drinkImage8,
   },
-
-  // {
-  //   title: "Connect with Us",
-  //   description:
-  //     "Join our community on Facebook and Instagram for inspiration, updates, and glimpses of the extraordinary events we create. We look forward to connecting with you and making your celebration one to remember.",
-
-  //   image: drinkImage9,
-  // },
-
-  // {
-  //   title: "Ready to Raise the Bar?",
-  //   description:
-  //     "Are you ready to take your event to new heights? Sign up for a personalized account and embark on an enchanting journey of flavor, elegance, and seamless service with Garnished. Tell us about your event by filling out a brief form with all the essential details. From your preferred drinks to the theme and unique requirements, your vision is the heart of our creation process.            Once we receive your event details, our dedicated team will craft a tailor-made quote just for you. We ensure transparency, allowing you to make an informed decision for your extraordinary celebration.",
-  //   image: drinkImage10,
-  // },
 ];
 
 export const Home: FC = () => {
@@ -127,7 +111,7 @@ export const Home: FC = () => {
       {/* about section */}
 
       <section className={`column ${styles.aboutSection}`}>
-        <ScrollAnimator type="FADE_GROW_IN" delay={1}>
+        <div>
           <h3 className={styles.heading}>Premium Mobile Bar</h3>
           <p className={styles.description}>
             Create unforgettable memories with Garnished, New England's premier
@@ -135,11 +119,11 @@ export const Home: FC = () => {
             wedding, or any other celebration, Garnished is here to turn it into
             an extraordinary experience.
           </p>
-        </ScrollAnimator>
+        </div>
 
-        <ScrollAnimator type="SLIDE_RIGHT" inViewPercent={0} delay={1.2}>
-          <BeerPourIcon />
-        </ScrollAnimator>
+        <div>
+          <img src={wine02} className={styles.aboutImage} alt="Drink" />
+        </div>
       </section>
 
       {/* selling points */}
@@ -148,10 +132,7 @@ export const Home: FC = () => {
       <section
         className={`content-spacer-large ${styles.sellingPointsSection}`}
       >
-        <ScrollAnimator
-          type="SLIDE_RIGHT"
-          className={`${styles.sellingPoints} column`}
-        >
+        <div className={`${styles.sellingPoints} column`}>
           <h3 className={styles.sellingPointHeading}>
             What Makes Us Different
           </h3>
@@ -159,20 +140,15 @@ export const Home: FC = () => {
           <div className={styles.sellingPointsWrapper}>
             {sellingPoints.map(({ title, description }, index) => {
               return (
-                <ScrollAnimator
-                  type="SLIDE_UP"
-                  delay={index * 0.2}
-                  className={styles.sellingPoint}
-                  key={title}
-                >
+                <div className={styles.sellingPoint} key={title}>
                   <CheckMarkIcon className={styles.checkMark} />
                   <h4 className={styles.subHeading}>{title}</h4>
                   <p className={styles.sellingPointDetails}>{description}</p>
-                </ScrollAnimator>
+                </div>
               );
             })}
           </div>
-        </ScrollAnimator>
+        </div>
       </section>
       <TiltDivider location="BOTTOM" className={styles.bottomDivider} />
 
@@ -186,26 +162,18 @@ export const Home: FC = () => {
                 index % 2 === 0 ? styles.section : styles.sectionReverse
               }
             >
-              <ScrollAnimator
-                type="SLIDE_RIGHT"
-                inViewPercent={0.75}
-                delay={0.2}
-              >
+              <div>
                 <h3 className={styles.heading}>{title}</h3>
                 <p className={styles.description}>{description}</p>
-              </ScrollAnimator>
+              </div>
 
-              <ScrollAnimator
-                type="FADE_GROW_IN"
-                inViewPercent={0.5}
-                delay={0.25}
-              >
+              <div>
                 <img
                   src={image}
                   className={styles.sectionImage}
                   alt="Specialty Drink"
                 />
-              </ScrollAnimator>
+              </div>
             </div>
           </section>
         );
