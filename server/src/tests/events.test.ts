6de0a194fd4a27c93f7cdb47499b171093c1d336
Bryan Miller <bmilcs@yahoo.com>
2023-08-28 17:32:41 -0400

    Add event delete tests to server

--- server/src/tests/events.test.ts
+++ server/src/tests/events.test.ts
@@ -1,5 +1,13 @@
+import EventModel from "@/models/event";
 import { TUserRequestDetails } from "@/models/user";
-import { app, eventData, request, signupUser, userData } from "@/tests/setup";
+import {
+  app,
+  createEvent,
+  eventData,
+  request,
+  signupUser,
+  userData,
+} from "@/tests/setup";
 import chai from "chai";
 const expect = chai.expect;
 
@@ -55,6 +63,47 @@ describe("Event Route: POST /event/", () => {
 //
 // event delete
 //
+
+describe("Event Route: DELETE /event/:id", () => {
+  it("Missing JWT cookies: 401 Unauthorized", async () => {
+    await request(app)
+      .delete("/event/123")
+      .expect("Content-Type", /json/)
+      .expect(401)
+      .then(res => {
+        expect(res.body.msg).to.equal("Unauthorized.");
+      });
+  });
+
+  it("With JWT cookies & invalid eventId: 404", async () => {
+    const cookies = await signupUser(app, userData as TUserRequestDetails);
+
+    await request(app)
+      .delete("/event/123")
+      .set("Cookie", cookies)
+      .expect("Content-Type", /json/)
+      .expect(404)
+      .then(res => {
+        expect(res.body.msg).to.equal("Event not found.");
+      });
+  });
+
+  it("With JWT cookies & valid eventId: 200 & database check", async () => {
+    const cookies = await signupUser(app, userData as TUserRequestDetails);
+    const eventId = await createEvent(app, cookies, eventData);
+
+    await request(app)
+      .delete(`/event/${eventId}`)
+      .set("Cookie", cookies)
+      .expect("Content-Type", /json/)
+      .expect(200, { msg: "Successful event delete." })
+      .then(async res => {
+        // check that event was deleted from database
+        const event = await EventModel.findById(eventId);
+        expect(event).to.equal(null);
+      });
+  });
+});
 
 //
 // event get
