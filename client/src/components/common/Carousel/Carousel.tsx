61c60120bd9b663f170a7dd71793ef9719679b79
Bryan Miller <bmilcs@yahoo.com>
2023-09-02 18:24:38 -0400

    Add carousel functionality w/ nav grid on client

--- client/src/components/common/Carousel/Carousel.tsx
+++ client/src/components/common/Carousel/Carousel.tsx
@@ -1,3 +1,6 @@
+import { ArrowSVG } from "@/components/common/ArrowSVG/ArrowSVG";
+import { Button } from "@/components/common/Button/Button";
+import { Modal } from "@/components/common/Modal/Modal";
 import { useWindowResize } from "@/hooks/useWindowResize";
 import { useEffect, useRef, useState } from "react";
 import styles from "./Carousel.module.scss";
@@ -7,35 +10,54 @@ type TProps = {
 };
 
 function Carousel({ imageArray }: TProps) {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const [carouselTotalWidth, setCarouselTotalWidth] = useState(0);
-  const [carouselVisibleWidth, setCarouselVisibleWidth] = useState(0);
-  const [slideWidth, setSlideWidth] = useState(0);
-  const [scrollPosition, setScrollPosition] = useState(0);
-  const carouselRef = useRef<HTMLDivElement>(null);
-  const slideRef = useRef<HTMLDivElement>(null);
   const windowSize = useWindowResize();
+  const carouselRef = useRef<HTMLDivElement>(null);
+  const carouselSlideRef = useRef<HTMLDivElement>(null);
+  const navRef = useRef<HTMLDivElement>(null);
+  const navSlideRef = useRef<HTMLDivElement>(null);
+  const [carouselSlideWidth, setCarouselSlideWidth] = useState(0);
+  const [navSlideWidth, setNavSlideWidth] = useState(0);
+  const [modalImage, setModalImage] = useState<string | null>(null);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-  useEffect(() => {
-    setCarouselTotalWidth(carouselRef.current?.scrollWidth ?? 0);
-    setCarouselVisibleWidth(carouselRef.current?.clientWidth ?? 0);
-    setSlideWidth(slideRef.current?.scrollWidth ?? 0);
-  }, [windowSize, carouselTotalWidth]);
+  useEffect(
+    function setSlideWidthsOnWindowSizeChange() {
+      setCarouselSlideWidth(carouselSlideRef.current?.scrollWidth ?? 0);
+      setNavSlideWidth(navSlideRef.current?.scrollWidth ?? 0);
+    },
+    [windowSize],
+  );
 
-  const handleScroll = (e: React.UIEvent<HTMLDivElement>) => {
+  useEffect(
+    function scrollToImagesOnIndexChange() {
+      const scrollAfterDebounce = setTimeout(() => {
+        const carouselScrollPosition = carouselSlideWidth * currentImageIndex;
+        carouselRef.current?.scrollTo({
+          left: carouselScrollPosition,
+          behavior: "smooth",
+        });
+
+        // scroll nav slide to center of the nav grid
+        const navGridWidth = navRef.current?.clientWidth ?? 0;
+        const navFarLeftScrollPosition = navSlideWidth * currentImageIndex;
+        const navFinalScrollPosition =
+          navFarLeftScrollPosition - (navGridWidth / 2 - navSlideWidth);
+        navRef.current?.scrollTo({
+          left: navFinalScrollPosition,
+          behavior: "smooth",
+        });
+      }, 100);
+
+      return () => clearTimeout(scrollAfterDebounce);
+    },
+    [currentImageIndex, carouselSlideWidth, navSlideWidth],
+  );
+
+  const handleCarouselScroll = (e: React.UIEvent<HTMLDivElement>) => {
     const scrollLeft = e.currentTarget.scrollLeft;
-    setScrollPosition(scrollLeft);
-    const slideWidthPercentage = slideWidth / carouselVisibleWidth;
-    const offset = scrollLeft * slideWidthPercentage;
-
-    console.log(
-      { scrollPosition: scrollLeft },
-      { carouselTotalWidth },
-      { carouselVisibleWidth },
-      { slideWidth },
-      { slideWidthPercentage },
-      { offset },
-    );
+    const index = scrollLeft / carouselSlideWidth;
+    if (index % 1 !== 0) return;
+    setCurrentImageIndex(index);
   };
 
   const handleNextImage = () => {
@@ -50,18 +72,100 @@ function Carousel({ imageArray }: TProps) {
       : setCurrentImageIndex(currentImageIndex - 1);
   };
 
+  const handleCarouselImageClick = (imageUrl: string) => {
+    setModalImage(imageUrl);
+  };
+
+  const handleModalImageClick = () => {
+    setModalImage(null);
+  };
+
+  const handleNavigationSlideClick = (index: number) => {
+    setCurrentImageIndex(index);
+  };
+
   return (
-    <div className={styles.carousel} ref={carouselRef} onScroll={handleScroll}>
-      {imageArray.map((image, index) => (
-        <div key={image} className={styles.carouselItem} ref={slideRef}>
-          <img
-            src={image}
-            onClick={() => setCurrentImageIndex(index)}
-            className={styles.carouselImage}
-          />
+    <>
+      {/* carousel */}
+
+      <div className={styles.carouselWrapper}>
+        <div
+          className={`${styles.carousel}`}
+          ref={carouselRef}
+          onScroll={handleCarouselScroll}
+        >
+          {imageArray.map(image => (
+            <div
+              key={image}
+              className={styles.carouselSlide}
+              ref={carouselSlideRef}
+            >
+              <img
+                src={image}
+                onClick={() => handleCarouselImageClick(image)}
+                alt={`Carousel Image ${image}`}
+                className={styles.carouselImage}
+              />
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+
+        {/* carousel controls */}
+        <div className={styles.carouselControls}>
+          <Button
+            type="icon"
+            onClick={handlePreviousImage}
+            className={styles.carouselArrowButton}
+            ariaLabel="Previous Image"
+          >
+            <ArrowSVG direction="left" className={styles.carouselArrowSVG} />
+          </Button>
+          <Button
+            type="icon"
+            onClick={handleNextImage}
+            className={styles.carouselArrowButton}
+            ariaLabel="Next Image"
+          >
+            <ArrowSVG direction="right" className={styles.carouselArrowSVG} />
+          </Button>
+        </div>
+      </div>
+
+      {/* image grid */}
+      <div className="">
+        <div className={styles.navigationGrid} ref={navRef}>
+          {imageArray.map((image, index) => (
+            <div ref={navSlideRef} key={`${image}-${index}`}>
+              <Button
+                type="icon"
+                className={styles.navigationSlide}
+                ariaLabel={`Image ${index + 1}`}
+              >
+                <img
+                  key={image}
+                  src={image}
+                  alt={`Navigation Image ${image}`}
+                  onClick={() => handleNavigationSlideClick(index)}
+                  className={`${styles.navigationSlideImage}${
+                    index === currentImageIndex
+                      ? ` ${styles.navigationSlideImageActive}`
+                      : ""
+                  }`}
+                />
+              </Button>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* modal */}
+
+      {modalImage !== null && (
+        <Modal onClick={() => handleModalImageClick()} type="image">
+          <img src={modalImage} alt="Modal Image" />
+        </Modal>
+      )}
+    </>
   );
 }
 
