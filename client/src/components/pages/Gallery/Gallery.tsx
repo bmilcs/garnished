fd323a22a0597edf7f17f267612ce67e7432b16e
Bryan Miller <bmilcs@yahoo.com>
2023-09-05 15:15:17 -0400

    Refactor Gallery page w/ new asset configuration on client

--- client/src/components/pages/Gallery/Gallery.tsx
+++ client/src/components/pages/Gallery/Gallery.tsx
@@ -1,28 +1,25 @@
-import backgroundImage from "@/assets/fruit-02.jpg";
-import * as imagesObject from "@/assets/index";
+// import * as imagesObject from "@/assets/index";
+import { drinks } from "@/assets";
 import Carousel from "@/components/common/Carousel/Carousel";
 import { Hero } from "@/components/common/Hero/Hero";
 import { FC } from "react";
 import styles from "./Gallery.module.scss";
 
 export const Gallery: FC = () => {
-  const imageUrls = Object.values(imagesObject);
-
   return (
     <>
       <Hero
-        backgroundImage={backgroundImage}
+        backgroundImage={drinks.specialty_drink_42.full}
         title="Photo"
         titleSpan="Gallery"
         subtitle="Eye candy from our events"
-        heightInVH={35}
       />
 
       {/* image grid */}
 
       <section className={`content-spacer ${styles.carouselSection}`}>
         <div className="column">
-          <Carousel imageArray={imageUrls} />
+          <Carousel imageObject={drinks} />
         </div>
       </section>
     </>
