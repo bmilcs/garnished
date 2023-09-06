283f7cb31160b3c58a2e187891be49e1e615ce3a
Bryan Miller <bmilcs@yahoo.com>
2023-09-06 17:50:09 -0400

    Add utils/navigation to client

---
+++ client/src/utils/navigation.ts
@@ -0,0 +1,22 @@
+export const navigationLinks = [
+  {
+    name: "Home",
+    link: "/",
+  },
+  {
+    name: "Services",
+    link: "/services",
+  },
+  {
+    name: "Seasonal Menus",
+    link: "/seasonal-menus",
+  },
+  {
+    name: "Gallery",
+    link: "/gallery",
+  },
+  {
+    name: "Contact",
+    link: "/contact",
+  },
+];
