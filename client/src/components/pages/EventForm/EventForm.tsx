9cea2e3abdb09a6b54042a5b48c6f9878d613c5d
Bryan Miller <bmilcs@yahoo.com>
2023-09-19 16:40:49 -0400

    Refactor Event Form using Input component on client

--- client/src/components/pages/EventForm/EventForm.tsx
+++ client/src/components/pages/EventForm/EventForm.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/common/Button/Button";
 import { HourglassSpinner } from "@/components/common/HourglassSpinner/HourglassSpinner";
+import { Input } from "@/components/common/Input/Input";
 import ScrollAnimator from "@/components/common/ScrollAnimator/ScrollAnimator";
 import { useCreateEvent } from "@/hooks/useEventCreate";
 import { useInputChange } from "@/hooks/useInputChange";
@@ -28,336 +29,190 @@ export const EventForm: FC = () => {
               <p>Receive a free estimate for your event.</p>
             </div>
 
-            <div className="input-group">
-              <label htmlFor="date">Event Date</label>
-              <input
-                type="date"
-                name="date"
-                id="date"
-                onChange={handleInputChange}
-                value={formData.date}
-                required
-              />
-              {errors.length > 0 && (
-                <p className="error">
-                  {getExpressValidatorError("date", errors)}
-                </p>
-              )}
-            </div>
+            <Input
+              name="date"
+              type="date"
+              label="Event Date"
+              onChange={handleInputChange}
+              value={formData.date}
+              required
+              error={getExpressValidatorError("date", errors)}
+            />
 
-            <div className="input-group">
-              <label htmlFor="time">Time</label>
-              <input
-                type="time"
-                name="time"
-                id="time"
-                onChange={handleInputChange}
-                value={formData.time}
-                required
-              />
-              {errors.length > 0 && (
-                <p className="error">
-                  {getExpressValidatorError("time", errors)}
-                </p>
-              )}
-            </div>
+            <Input
+              name="time"
+              type="time"
+              label="Event Starting Time"
+              onChange={handleInputChange}
+              value={formData.time}
+              required
+              error={getExpressValidatorError("time", errors)}
+            />
 
-            <div className="input-group">
-              <label htmlFor="locationDescription">Location Description</label>
-              <input
-                type="text"
-                name="locationDescription"
-                id="locationDescription"
-                onChange={handleInputChange}
-                value={formData.locationDescription}
-                required
-              />
-              {errors.length > 0 && (
-                <p className="error">
-                  {getExpressValidatorError("locationDescription", errors)}
-                </p>
-              )}
-            </div>
+            <Input
+              name="address"
+              type="text"
+              label="Event Address"
+              onChange={handleInputChange}
+              value={formData.address}
+              required
+              error={getExpressValidatorError("address", errors)}
+            />
 
-            <div className="input-group">
-              <label htmlFor="address">Address</label>
-              <input
-                type="text"
-                name="address"
-                id="address"
-                onChange={handleInputChange}
-                value={formData.address}
-                required
-              />
-              {errors.length > 0 && (
-                <p className="error">
-                  {getExpressValidatorError("address", errors)}
-                </p>
-              )}
-            </div>
+            <Input
+              name="city"
+              type="text"
+              label="Event City"
+              onChange={handleInputChange}
+              value={formData.city}
+              required
+              error={getExpressValidatorError("city", errors)}
+            />
 
-            <div className="input-group">
-              <label htmlFor="city">City</label>
-              <input
-                type="text"
-                name="city"
-                id="city"
-                onChange={handleInputChange}
-                value={formData.city}
-                required
-              />
-              {errors.length > 0 && (
-                <p className="error">
-                  {getExpressValidatorError("city", errors)}
-                </p>
-              )}
-            </div>
+            <Input
+              name="state"
+              type="text"
+              label="Event State"
+              onChange={handleInputChange}
+              value={formData.state}
+              maxLength={2}
+              required
+              error={getExpressValidatorError("state", errors)}
+            />
 
-            <div className="input-group">
-              <label htmlFor="state">State</label>
-              <input
-                type="text"
-                name="state"
-                id="state"
-                onChange={handleInputChange}
-                value={formData.state}
-                required
-              />
-              {errors.length > 0 && (
-                <p className="error">
-                  {getExpressValidatorError("state", errors)}
-                </p>
-              )}
-            </div>
+            <Input
+              name="zip"
+              type="text"
+              label="Event Zip Code"
+              onChange={handleInputChange}
+              value={formData.zip || ""}
+              maxLength={2}
+              required
+              error={getExpressValidatorError("zip", errors)}
+            />
 
-            <div className="input-group">
-              <label htmlFor="zip">Zip Code</label>
-              <input
-                type="text"
-                name="zip"
-                id="zip"
-                maxLength={5}
-                onChange={handleInputChange}
-                value={formData.zip || ""}
-                required
-              />
-              {errors.length > 0 && (
-                <p className="error">
-                  {getExpressValidatorError("zip", errors)}
-                </p>
-              )}
-            </div>
+            <Input
+              name="locationDescription"
+              type="text"
+              label="Directions / Location Description"
+              onChange={handleInputChange}
+              value={formData.locationDescription}
+              required
+              error={getExpressValidatorError("locationDescription", errors)}
+            />
 
-            <div className="input-group">
-              <label htmlFor="guests">Number of Guests</label>
-              <input
-                type="text"
-                name="guests"
-                id="guests"
-                maxLength={5}
-                onChange={handleInputChange}
-                value={formData.guests || ""}
-                required
-              />
-              {errors.length > 0 && (
-                <p className="error">
-                  {getExpressValidatorError("guests", errors)}
-                </p>
-              )}
-            </div>
+            <Input
+              name="eventType"
+              type="text"
+              label="Type of Event"
+              onChange={handleInputChange}
+              value={formData.eventType}
+              required
+              error={getExpressValidatorError("eventType", errors)}
+            />
 
-            <div className="input-group">
-              <label htmlFor="hours">Number of Hours</label>
-              <input
-                type="text"
-                name="hours"
-                id="hours"
-                maxLength={5}
-                onChange={handleInputChange}
-                value={formData.hours || ""}
-                required
-              />
-              {errors.length > 0 && (
-                <p className="error">
-                  {getExpressValidatorError("hours", errors)}
-                </p>
-              )}
-            </div>
+            <Input
+              name="guests"
+              type="number"
+              label="Guest Count"
+              onChange={handleInputChange}
+              value={formData.guests}
+              maxLength={2}
+              required
+              error={getExpressValidatorError("guests", errors)}
+            />
 
-            <div className="input-group">
-              <label htmlFor="eventType">Event Type</label>
-              <input
-                type="text"
-                name="eventType"
-                id="eventType"
-                maxLength={5}
-                onChange={handleInputChange}
-                value={formData.eventType}
-                required
-              />
-              {errors.length > 0 && (
-                <p className="error">
-                  {getExpressValidatorError("eventType", errors)}
-                </p>
-              )}
-            </div>
+            <Input
+              name="hours"
+              type="number"
+              label="Number of Hours"
+              onChange={handleInputChange}
+              value={formData.hours}
+              maxLength={2}
+              required
+              error={getExpressValidatorError("hours", errors)}
+            />
+
+            <label>What services do you need?</label>
 
             <fieldset>
-              <legend>What services do you need?</legend>
+              <Input
+                name="needBar"
+                type="checkbox"
+                label="Mobile Bar"
+                onChange={handleInputChange}
+                checked={formData.needBar}
+                error={getExpressValidatorError("needBar", errors)}
+              />
 
-              <div className="input-group">
-                <label htmlFor="needBar">
-                  <input
-                    type="checkbox"
-                    name="needBar"
-                    id="needBar"
-                    maxLength={5}
-                    onChange={handleInputChange}
-                    checked={formData.needBar ? true : false}
-                  />
-                  Mobile Bar
-                </label>
-                {errors.length > 0 && (
-                  <p className="error">
-                    {getExpressValidatorError("needBar", errors)}
-                  </p>
-                )}
-              </div>
+              <Input
+                name="needTent"
+                type="checkbox"
+                label="Tent"
+                onChange={handleInputChange}
+                checked={formData.needTent}
+                error={getExpressValidatorError("needTent", errors)}
+              />
 
-              <div className="input-group">
-                <label htmlFor="needTent">
-                  <input
-                    type="checkbox"
-                    name="needTent"
-                    id="needTent"
-                    maxLength={5}
-                    onChange={handleInputChange}
-                    checked={formData.needTent ? true : false}
-                  />
-                  Tent
-                </label>
-                {errors.length > 0 && (
-                  <p className="error">
-                    {getExpressValidatorError("needTent", errors)}
-                  </p>
-                )}
-              </div>
+              <Input
+                name="needAlcohol"
+                type="checkbox"
+                label="Alcohol"
+                onChange={handleInputChange}
+                checked={formData.needAlcohol}
+                error={getExpressValidatorError("needAlcohol", errors)}
+              />
 
-              <div className="input-group">
-                <label htmlFor="needAlcohol">
-                  <input
-                    type="checkbox"
-                    name="needAlcohol"
-                    id="needAlcohol"
-                    maxLength={5}
-                    onChange={handleInputChange}
-                    checked={formData.needAlcohol ? true : false}
-                  />
-                  Alcohol
-                </label>
-                {errors.length > 0 && (
-                  <p className="error">
-                    {getExpressValidatorError("needAlcohol", errors)}
-                  </p>
-                )}
-              </div>
-
-              <div className="input-group">
-                <label htmlFor="needDrinkware">
-                  <input
-                    type="checkbox"
-                    name="needDrinkware"
-                    id="needDrinkware"
-                    maxLength={5}
-                    onChange={handleInputChange}
-                    checked={formData.needDrinkware ? true : false}
-                  />
-                  Disposable Drinkware
-                </label>
-                {errors.length > 0 && (
-                  <p className="error">
-                    {getExpressValidatorError("needDrinkware", errors)}
-                  </p>
-                )}
-              </div>
+              <Input
+                name="needDrinkware"
+                type="checkbox"
+                label="Disposable Drinkware"
+                onChange={handleInputChange}
+                checked={formData.needDrinkware}
+                error={getExpressValidatorError("needDrinkware", errors)}
+              />
             </fieldset>
+
+            <label>What beverages do you want to serve?</label>
 
             <fieldset>
-              <legend>What beverages do you want to serve?</legend>
-              <div className="input-group">
-                <label htmlFor="beer">
-                  <input
-                    type="checkbox"
-                    name="beer"
-                    id="beer"
-                    maxLength={5}
-                    onChange={handleInputChange}
-                    checked={formData.beer ? true : false}
-                  />
-                  Beer
-                </label>
-                {errors.length > 0 && (
-                  <p className="error">
-                    {getExpressValidatorError("beer", errors)}
-                  </p>
-                )}
-              </div>
+              <Input
+                name="beer"
+                type="checkbox"
+                label="Beer"
+                onChange={handleInputChange}
+                checked={formData.beer}
+                error={getExpressValidatorError("beer", errors)}
+              />
 
-              <div className="input-group">
-                <label htmlFor="wine">
-                  <input
-                    type="checkbox"
-                    name="wine"
-                    id="wine"
-                    maxLength={5}
-                    onChange={handleInputChange}
-                    checked={formData.wine ? true : false}
-                  />
-                  Wine
-                </label>
-                {errors.length > 0 && (
-                  <p className="error">
-                    {getExpressValidatorError("wine", errors)}
-                  </p>
-                )}
-              </div>
+              <Input
+                name="wine"
+                type="checkbox"
+                label="Wine"
+                onChange={handleInputChange}
+                checked={formData.wine}
+                error={getExpressValidatorError("wine", errors)}
+              />
 
-              <div className="input-group">
-                <label htmlFor="specialtyDrinks">
-                  <input
-                    type="checkbox"
-                    name="specialtyDrinks"
-                    id="specialtyDrinks"
-                    maxLength={5}
-                    onChange={handleInputChange}
-                    checked={formData.specialtyDrinks ? true : false}
-                  />
-                  Specialty Drinks
-                </label>
-                {errors.length > 0 && (
-                  <p className="error">
-                    {getExpressValidatorError("specialtyDrinks", errors)}
-                  </p>
-                )}
-              </div>
-
-              <div className="input-group">
-                <label htmlFor="liquorPreferences">Liquor Preferences</label>
-                <input
-                  type="text"
-                  name="liquorPreferences"
-                  id="liquorPreferences"
-                  maxLength={200}
-                  onChange={handleInputChange}
-                  value={formData.liquorPreferences}
-                />
-                {errors.length > 0 && (
-                  <p className="error">
-                    {getExpressValidatorError("liquorPreferences", errors)}
-                  </p>
-                )}
-              </div>
+              <Input
+                name="specialtyDrinks"
+                type="checkbox"
+                label="Specialty Drinks"
+                onChange={handleInputChange}
+                checked={formData.specialtyDrinks}
+                error={getExpressValidatorError("specialtyDrinks", errors)}
+              />
             </fieldset>
+
+            <Input
+              name="liquorPreferences"
+              type="textarea"
+              label="Liquor Preferences (optional)"
+              maxLength={200}
+              onChange={handleInputChange}
+              value={formData.liquorPreferences}
+              error={getExpressValidatorError("liquorPreferences", errors)}
+            />
 
             <Button type="primary">Submit Estimate Request</Button>
           </form>
