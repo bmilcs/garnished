fbd5b43922c9a581a61aaddc7b0441377d4d2cf7
Bryan Miller <bmilcs@yahoo.com>
2023-09-21 17:00:35 -0400

    Add event update patch tests to server

--- server/src/tests/events.test.ts
+++ server/src/tests/events.test.ts
@@ -153,3 +153,68 @@ describe("Event Route: GET /event/:id", () => {
 //
 // event update
 //
+
+describe("Event Route: PATCH /event/:id", () => {
+  it("Missing JWT cookies: 401 Unauthorized", async () => {
+    await request(app)
+      .patch("/event/123")
+      .expect("Content-Type", /json/)
+      .expect(401)
+      .then(res => {
+        expect(res.body.msg).to.equal("Unauthorized.");
+      });
+  });
+
+  it("With JWT cookies & invalid eventId: 404", async () => {
+    const cookies = await signupUser(app, userData as TUserRequestDetails);
+
+    await request(app)
+      .patch("/event/123")
+      .set("Cookie", cookies)
+      .expect("Content-Type", /json/)
+      .expect(404)
+      .then(res => {
+        expect(res.body.msg).to.equal("Event not found.");
+      });
+  });
+
+  it("With JWT cookies & valid eventId & invalid data: 400", async () => {
+    const cookies = await signupUser(app, userData as TUserRequestDetails);
+    const eventId = await createEvent(app, cookies, eventData);
+
+    await request(app)
+      .patch(`/event/${eventId}`)
+      .set("Cookie", cookies)
+      .send({ ...eventData, beer: "yes" })
+      .expect("Content-Type", /json/)
+      .expect(400)
+      .then(res => {
+        expect(res.body.msg).to.equal("Failed to validate event data.");
+        expect(res.body.errors).to.be.an("array");
+        expect(res.body.errors[0].msg).to.equal(
+          "A valid beer need is required.",
+        );
+      });
+  });
+
+  it("With JWT cookies & valid eventId & valid data: 200 & database check", async () => {
+    const cookies = await signupUser(app, userData as TUserRequestDetails);
+    const eventId = await createEvent(app, cookies, eventData);
+
+    await request(app)
+      .patch(`/event/${eventId}`)
+      .set("Cookie", cookies)
+      .send({ ...eventData, address: "321 Updated Dr." })
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then(async res => {
+        expect(res.body).to.contain({ msg: "Successful event update." });
+        expect(res.body.updated).to.equal(true);
+
+        // check that event was updated in database
+        const event = await EventModel.findById(eventId);
+        expect(event).to.be.an("object");
+        expect(event?.address).to.equal("321 Updated Dr.");
+      });
+  });
+});
