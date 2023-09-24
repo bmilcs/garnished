980e4bde390297f22eec71e66d99866960f42246
Bryan Miller <bmilcs@yahoo.com>
2023-09-24 13:20:54 -0400

    Rename ArrowSVG to ArrowIcon for consistency on client

--- client/src/components/common/ArrowIcon/ArrowIcon.tsx
+++ client/src/components/common/ArrowIcon/ArrowIcon.tsx
@@ -5,12 +5,12 @@ type TArrowDirection = "left" | "right";
 
 type TProps = TClassName & { direction: TArrowDirection };
 
-export const ArrowSVG: FC<TProps> = ({ direction = "left", className }) => {
+export const ArrowIcon: FC<TProps> = ({ direction = "left", className }) => {
   return direction === "left" ? (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 20 20"
-      className={className ?? ""}
+      className={className}
     >
       <path fill="currentColor" d="m4 10l9 9l1.4-1.5L7 10l7.4-7.5L13 1z" />
     </svg>
@@ -18,7 +18,7 @@ export const ArrowSVG: FC<TProps> = ({ direction = "left", className }) => {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 20 20"
-      className={className ?? ""}
+      className={className}
     >
       <path fill="currentColor" d="M7 1L5.6 2.5L13 10l-7.4 7.5L7 19l9-9z" />
     </svg>
