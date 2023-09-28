21659e07b46948c9c0dbe0a7ff72a2303de01f64
Bryan Miller <bmilcs@yahoo.com>
2023-09-28 15:50:10 -0400

    Refactor PageTransition using animation utils global var

--- client/src/components/common/PageTransition/PageTransition.tsx
+++ client/src/components/common/PageTransition/PageTransition.tsx
@@ -1,4 +1,5 @@
 import { TClassName } from "@/types/propTypes";
+import { PAGE_TRANSITION_DURATION } from "@/utils/animations";
 import { motion } from "framer-motion";
 import React, { FC } from "react";
 
@@ -13,7 +14,7 @@ export const PageTransition: FC<TProps> = ({ children, className }) => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      transition={{ ease: "easeInOut", duration: 0.5 }}
+      transition={{ ease: "easeInOut", duration: PAGE_TRANSITION_DURATION }}
     >
       {children}
     </motion.main>
