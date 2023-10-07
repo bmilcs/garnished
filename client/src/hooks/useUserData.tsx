0670c180d8f925aed0c4e57f2363c64fa9c6484e
Bryan Miller <bmilcs@yahoo.com>
2023-10-07 15:25:08 -0400

    Improve readability & comments in useUserData hook

--- client/src/hooks/useUserData.tsx
+++ client/src/hooks/useUserData.tsx
@@ -9,6 +9,9 @@ export type TUserApiResponse = {
   user: TUser;
 };
 
+// this hook is used by User & UserUpdate page. it fetches user data from server, and provides
+// state variables for user data, pending status, and error messages.
+
 export const useUserData = () => {
   const { redirectUnauthorizedUser, setIsLoggedIn } = useContext(AuthContext);
   const [isPending, setIsPending] = useState(true);
@@ -16,30 +19,31 @@ export const useUserData = () => {
   const [error, setError] = useState("");
 
   // on initial render, retrieve user data from server
+
   useEffect(() => {
     redirectUnauthorizedUser();
 
     const getUserData = async () => {
       setIsPending(true);
       setError("");
-
       try {
         const {
           data: { user },
         } = await apiService<TUserApiResponse>({
           path: "user",
         });
-
-        // if no user data is returned, the server clears jwt cookies & logs the user out.
-        // update the GUI accordingly by setting isLoggedIn to false, hiding user dashboard access.
-        // the user dashboard page will then display the ErrorPage component.
         if (user) {
           const formattedUser = {
             ...user,
             phone: formatPhoneNumber(user.phone),
           };
           setUserData(formattedUser);
-        } else setIsLoggedIn(false);
+          return;
+        }
+        // if no user data is found, the server clears jwt cookies & logs the user out.
+        // update the GUI accordingly by setting isLoggedIn to false, hiding user dashboard access.
+        // the user dashboard page will then display the ErrorPage component.
+        setIsLoggedIn(false);
       } catch {
         setError("Something went wrong while logging in. Try again later.");
         setIsLoggedIn(false);
