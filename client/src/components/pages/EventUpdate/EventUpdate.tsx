c648b7a9cf2e4cf452436f6ff211108d7efb2835
Bryan Miller <bmilcs@yahoo.com>
2023-10-07 17:06:12 -0400

    Refactor EventUpdate page using updated useEventUpdate hook

--- client/src/components/pages/EventUpdate/EventUpdate.tsx
+++ client/src/components/pages/EventUpdate/EventUpdate.tsx
@@ -8,285 +8,299 @@ import { useEventUpdate } from "@/hooks/useEventUpdate";
 import { useInputChange } from "@/hooks/useInputChange";
 import { getExpressValidatorError, onFormSubmit } from "@/utils/forms";
 import { FC, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 export const EventUpdate: FC = () => {
   const { id: eventId } = useParams();
+  const navigate = useNavigate();
   const {
-    formData,
-    setFormData,
     updateEvent,
     deleteEvent,
-    isPending,
+    setFormData,
+    formData,
     updateErrors,
     deleteError,
+    eventDataError,
+    isEventActionPending,
+    isEventDataPending,
   } = useEventUpdate(eventId ?? "");
   const handleSubmitForm = onFormSubmit(updateEvent);
   const handleInputChange = useInputChange(setFormData);
   const [showDeleteModal, setShowDeleteModal] = useState(false);
 
-  if (isPending) return <HourglassSpinner />;
+  // pending status: fetch event data, delete event or update event
 
-  if (!isPending && !formData)
+  if (isEventDataPending || isEventActionPending) {
+    return <HourglassSpinner />;
+  }
+
+  // error status: event data fetch error or form data is missing
+  // after pending status is set to false
+
+  if (eventDataError || !formData) {
     return <ErrorPage title="404" subtitle="Event not found." />;
+  }
 
-  if (formData)
-    return (
-      <section className="content-spacer user-section">
-        <AnimatedDiv type="SLIDE_DOWN" className="column">
-          <form onSubmit={handleSubmitForm}>
-            <div className="form-header">
-              <h2>Update Event Info</h2>
-              <p>Need to make some changes? Update your event below:</p>
-            </div>
+  // success status: no pending or error statuses present
 
+  return (
+    <section className="content-spacer user-section">
+      <AnimatedDiv type="SLIDE_DOWN" className="column">
+        <form onSubmit={handleSubmitForm}>
+          <div className="form-header">
+            <h2>Update Event Info</h2>
+            <p>Need to make some changes? Update your event below:</p>
+          </div>
+
+          <Input
+            name="date"
+            type="date"
+            label="Event Date"
+            onChange={handleInputChange}
+            value={formData.date}
+            required
+            error={getExpressValidatorError("date", updateErrors)}
+          />
+
+          <Input
+            name="time"
+            type="time"
+            label="Event Starting Time"
+            onChange={handleInputChange}
+            value={formData.time}
+            required
+            error={getExpressValidatorError("time", updateErrors)}
+          />
+
+          <Input
+            name="address"
+            type="text"
+            label="Event Address"
+            onChange={handleInputChange}
+            value={formData.address}
+            required
+            error={getExpressValidatorError("address", updateErrors)}
+          />
+
+          <Input
+            name="city"
+            type="text"
+            label="Event City"
+            onChange={handleInputChange}
+            value={formData.city}
+            required
+            error={getExpressValidatorError("city", updateErrors)}
+          />
+
+          <Input
+            name="state"
+            type="text"
+            label="Event State"
+            onChange={handleInputChange}
+            value={formData.state}
+            maxLength={2}
+            required
+            error={getExpressValidatorError("state", updateErrors)}
+          />
+
+          <Input
+            name="zip"
+            type="text"
+            label="Event Zip Code"
+            onChange={handleInputChange}
+            value={formData.zip || ""}
+            maxLength={2}
+            required
+            error={getExpressValidatorError("zip", updateErrors)}
+          />
+
+          <Input
+            name="locationDescription"
+            type="text"
+            label="Directions / Location Description"
+            onChange={handleInputChange}
+            value={formData.locationDescription}
+            required
+            error={getExpressValidatorError(
+              "locationDescription",
+              updateErrors,
+            )}
+          />
+
+          <Input
+            name="eventType"
+            type="text"
+            label="Type of Event"
+            onChange={handleInputChange}
+            value={formData.eventType}
+            required
+            error={getExpressValidatorError("eventType", updateErrors)}
+          />
+
+          <Input
+            name="guests"
+            type="number"
+            label="Guest Count"
+            onChange={handleInputChange}
+            value={formData.guests}
+            maxLength={2}
+            required
+            error={getExpressValidatorError("guests", updateErrors)}
+          />
+
+          <Input
+            name="hours"
+            type="number"
+            label="Number of Hours"
+            onChange={handleInputChange}
+            value={formData.hours}
+            maxLength={2}
+            required
+            error={getExpressValidatorError("hours", updateErrors)}
+          />
+
+          <label>What services do you need?</label>
+
+          <fieldset>
             <Input
-              name="date"
-              type="date"
-              label="Event Date"
+              name="needBar"
+              type="checkbox"
+              label="Mobile Bar"
               onChange={handleInputChange}
-              value={formData.date}
-              required
-              error={getExpressValidatorError("date", updateErrors)}
+              checked={formData.needBar}
+              error={getExpressValidatorError("needBar", updateErrors)}
             />
 
             <Input
-              name="time"
-              type="time"
-              label="Event Starting Time"
+              name="needTent"
+              type="checkbox"
+              label="Tent"
               onChange={handleInputChange}
-              value={formData.time}
-              required
-              error={getExpressValidatorError("time", updateErrors)}
+              checked={formData.needTent}
+              error={getExpressValidatorError("needTent", updateErrors)}
             />
 
             <Input
-              name="address"
-              type="text"
-              label="Event Address"
+              name="needAlcohol"
+              type="checkbox"
+              label="Alcohol"
               onChange={handleInputChange}
-              value={formData.address}
-              required
-              error={getExpressValidatorError("address", updateErrors)}
+              checked={formData.needAlcohol}
+              error={getExpressValidatorError("needAlcohol", updateErrors)}
             />
 
             <Input
-              name="city"
-              type="text"
-              label="Event City"
+              name="needDrinkware"
+              type="checkbox"
+              label="Disposable Drinkware"
               onChange={handleInputChange}
-              value={formData.city}
-              required
-              error={getExpressValidatorError("city", updateErrors)}
+              checked={formData.needDrinkware}
+              error={getExpressValidatorError("needDrinkware", updateErrors)}
+            />
+          </fieldset>
+
+          <label>What beverages do you want to serve?</label>
+
+          <fieldset>
+            <Input
+              name="beer"
+              type="checkbox"
+              label="Beer"
+              onChange={handleInputChange}
+              checked={formData.beer}
+              error={getExpressValidatorError("beer", updateErrors)}
             />
 
             <Input
-              name="state"
-              type="text"
-              label="Event State"
+              name="wine"
+              type="checkbox"
+              label="Wine"
               onChange={handleInputChange}
-              value={formData.state}
-              maxLength={2}
-              required
-              error={getExpressValidatorError("state", updateErrors)}
+              checked={formData.wine}
+              error={getExpressValidatorError("wine", updateErrors)}
             />
 
             <Input
-              name="zip"
-              type="text"
-              label="Event Zip Code"
+              name="specialtyDrinks"
+              type="checkbox"
+              label="Specialty Drinks"
               onChange={handleInputChange}
-              value={formData.zip || ""}
-              maxLength={2}
-              required
-              error={getExpressValidatorError("zip", updateErrors)}
+              checked={formData.specialtyDrinks}
+              error={getExpressValidatorError("specialtyDrinks", updateErrors)}
             />
+          </fieldset>
 
-            <Input
-              name="locationDescription"
-              type="text"
-              label="Directions / Location Description"
-              onChange={handleInputChange}
-              value={formData.locationDescription}
-              required
-              error={getExpressValidatorError(
-                "locationDescription",
-                updateErrors,
-              )}
-            />
+          <Input
+            name="liquorPreferences"
+            type="textarea"
+            label="Liquor Preferences (optional)"
+            maxLength={300}
+            onChange={handleInputChange}
+            value={formData.liquorPreferences}
+            error={getExpressValidatorError("liquorPreferences", updateErrors)}
+          />
 
-            <Input
-              name="eventType"
-              type="text"
-              label="Type of Event"
-              onChange={handleInputChange}
-              value={formData.eventType}
-              required
-              error={getExpressValidatorError("eventType", updateErrors)}
-            />
+          <Input
+            name="additionalInfo"
+            type="textarea"
+            label="Additional Information (optional)"
+            maxLength={1000}
+            onChange={handleInputChange}
+            value={formData.additionalInfo}
+            error={getExpressValidatorError("additionalInfo", updateErrors)}
+          />
 
-            <Input
-              name="guests"
-              type="number"
-              label="Guest Count"
-              onChange={handleInputChange}
-              value={formData.guests}
-              maxLength={2}
-              required
-              error={getExpressValidatorError("guests", updateErrors)}
-            />
+          <Button type="primary">Update Event Details</Button>
+        </form>
+      </AnimatedDiv>
 
-            <Input
-              name="hours"
-              type="number"
-              label="Number of Hours"
-              onChange={handleInputChange}
-              value={formData.hours}
-              maxLength={2}
-              required
-              error={getExpressValidatorError("hours", updateErrors)}
-            />
+      {/* bottom buttons */}
 
-            <label>What services do you need?</label>
-
-            <fieldset>
-              <Input
-                name="needBar"
-                type="checkbox"
-                label="Mobile Bar"
-                onChange={handleInputChange}
-                checked={formData.needBar}
-                error={getExpressValidatorError("needBar", updateErrors)}
-              />
-
-              <Input
-                name="needTent"
-                type="checkbox"
-                label="Tent"
-                onChange={handleInputChange}
-                checked={formData.needTent}
-                error={getExpressValidatorError("needTent", updateErrors)}
-              />
-
-              <Input
-                name="needAlcohol"
-                type="checkbox"
-                label="Alcohol"
-                onChange={handleInputChange}
-                checked={formData.needAlcohol}
-                error={getExpressValidatorError("needAlcohol", updateErrors)}
-              />
-
-              <Input
-                name="needDrinkware"
-                type="checkbox"
-                label="Disposable Drinkware"
-                onChange={handleInputChange}
-                checked={formData.needDrinkware}
-                error={getExpressValidatorError("needDrinkware", updateErrors)}
-              />
-            </fieldset>
-
-            <label>What beverages do you want to serve?</label>
-
-            <fieldset>
-              <Input
-                name="beer"
-                type="checkbox"
-                label="Beer"
-                onChange={handleInputChange}
-                checked={formData.beer}
-                error={getExpressValidatorError("beer", updateErrors)}
-              />
-
-              <Input
-                name="wine"
-                type="checkbox"
-                label="Wine"
-                onChange={handleInputChange}
-                checked={formData.wine}
-                error={getExpressValidatorError("wine", updateErrors)}
-              />
-
-              <Input
-                name="specialtyDrinks"
-                type="checkbox"
-                label="Specialty Drinks"
-                onChange={handleInputChange}
-                checked={formData.specialtyDrinks}
-                error={getExpressValidatorError(
-                  "specialtyDrinks",
-                  updateErrors,
-                )}
-              />
-            </fieldset>
-
-            <Input
-              name="liquorPreferences"
-              type="textarea"
-              label="Liquor Preferences (optional)"
-              maxLength={300}
-              onChange={handleInputChange}
-              value={formData.liquorPreferences}
-              error={getExpressValidatorError(
-                "liquorPreferences",
-                updateErrors,
-              )}
-            />
-
-            <Input
-              name="additionalInfo"
-              type="textarea"
-              label="Additional Information (optional)"
-              maxLength={1000}
-              onChange={handleInputChange}
-              value={formData.additionalInfo}
-              error={getExpressValidatorError("additionalInfo", updateErrors)}
-            />
-
-            <Button type="primary">Update Event Details</Button>
-          </form>
-        </AnimatedDiv>
-
+      <AnimatedDiv
+        when="IN_VIEW"
+        type="SLIDE_UP"
+        delay={0.2}
+        className="button-wrapper"
+      >
+        <Button type="outline" onClick={() => navigate("/user")}>
+          Return to Dashboard
+        </Button>
         <Button type="outline" onClick={() => setShowDeleteModal(true)}>
           Delete Event
         </Button>
+      </AnimatedDiv>
 
-        {!deleteError && showDeleteModal && (
-          <Modal
-            title="Delete Event?"
-            message="Are you sure you want to delete your event? This action cannot be undone."
-          >
-            <div className="button-wrapper">
-              <Button
-                type="outline"
-                onClick={deleteEvent}
-                className="margin-right"
-              >
-                Delete Event
-              </Button>
-              <Button
-                type="primary"
-                onClick={() => setShowDeleteModal(false)}
-                className="margin-right"
-              >
-                Cancel
-              </Button>
-            </div>
-          </Modal>
-        )}
+      {/* modal: confirm event delete */}
 
-        {deleteError && (
-          <Modal
-            title="Error"
-            message="Something went wrong while trying to delete your event. Please try again later."
-            onClickCloseOrOverlay={() => setShowDeleteModal(false)}
-          >
+      {!deleteError && showDeleteModal && (
+        <Modal
+          title="Delete Event?"
+          message="Are you sure you want to delete your event? This action cannot be undone."
+        >
+          <div className="button-wrapper">
+            <Button type="outline" onClick={deleteEvent}>
+              Delete Event
+            </Button>
             <Button type="primary" onClick={() => setShowDeleteModal(false)}>
               Cancel
             </Button>
-          </Modal>
-        )}
-      </section>
-    );
+          </div>
+        </Modal>
+      )}
+
+      {/* modal: delete event error */}
+
+      {deleteError && (
+        <Modal
+          title="Error"
+          message="Something went wrong while trying to delete your event. Please try again later."
+          onClickCloseOrOverlay={() => setShowDeleteModal(false)}
+        >
+          <Button type="primary" onClick={() => setShowDeleteModal(false)}>
+            Cancel
+          </Button>
+        </Modal>
+      )}
+    </section>
+  );
 };
