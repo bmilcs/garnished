39784f65b20589d316bc2667506edd435376d3d1
Bryan Miller <bmilcs@yahoo.com>
2023-10-11 18:27:01 -0400

    Refactor loginLimiter for playwright tests on client

--- server/src/middlewares/rateLimits.ts
+++ server/src/middlewares/rateLimits.ts
@@ -2,17 +2,25 @@ import {
   FAILED_LOGIN_ATTEMPTS_LIMIT,
   FAILED_LOGIN_TIME_WINDOW_IN_SECONDS,
 } from "@/config/env";
+import crypto from "crypto";
 import rateLimiter from "express-rate-limit";
 
 export const loginLimiter = rateLimiter({
   max: FAILED_LOGIN_ATTEMPTS_LIMIT,
   windowMs: FAILED_LOGIN_TIME_WINDOW_IN_SECONDS * 60 * 1000,
-  handler: function (req, res /*, next*/) {
+  handler: function (req, res) {
     res
       .status(429)
       .json({ msg: "Too many failed login attempts. Try again later." });
   },
-  keyGenerator: function (req /*, res*/) {
+  keyGenerator: function (req) {
+    // allow playwright authentication tests
+    const fullUrl = `${req.protocol}://${req.get("host")}${req.originalUrl}`;
+    const localTest = fullUrl === "http://localhost:3001/user/login";
+    if (localTest) {
+      const randomKey = crypto.randomBytes(16).toString("hex");
+      return randomKey;
+    }
     return req.body.username;
   },
 });
