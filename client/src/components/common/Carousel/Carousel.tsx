eedacfc66247b7e06b622510efdb7338fdbe1f7e
Bryan Miller <bmilcs@yahoo.com>
2023-10-15 16:22:39 -0400

    Refactor Carousel: auto-scroll, comments & consistent performance

--- client/src/components/common/Carousel/Carousel.tsx
+++ client/src/components/common/Carousel/Carousel.tsx
@@ -16,18 +16,73 @@ type TProps = {
 };
 
 function Carousel({ imageObject }: TProps) {
+  const MANUAL_SCROLL_DEBOUNCE_TIME_IN_MS = 200;
+  const AUTO_SCROLL_DELAY_DEFAULT_IN_MS = 2500;
+  const AUTO_SCROLL_DELAY_AFTER_USER_INTERACTION_IN_MS =
+    AUTO_SCROLL_DELAY_DEFAULT_IN_MS * 1.5;
   const imageObjectArray = Object.values(imageObject);
-  const [windowWidth] = useWindowResize();
   const carouselRef = useRef<HTMLDivElement>(null);
   const carouselSlideRef = useRef<HTMLDivElement>(null);
   const navRef = useRef<HTMLDivElement>(null);
   const navSlideRef = useRef<HTMLDivElement>(null);
+  const [windowWidth] = useWindowResize();
   const [carouselSlideWidth, setCarouselSlideWidth] = useState(0);
   const [navSlideWidth, setNavSlideWidth] = useState(0);
   const [modalImage, setModalImage] = useState<TResponsiveImage | null>(null);
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [carouselScrollPosition, setCarouselScrollPosition] = useState(0);
-  const DEBOUNCE_TIME = 100;
+  const [autoScrollDelay, setAutoScrollDelay] = useState(3000);
+
+  // save carousel scroll position on scroll. this is used to determine the
+  // current image index when the user scrolls the carousel manually.
+
+  const handleCarouselScroll = (e: React.UIEvent<HTMLDivElement>) => {
+    const scrollLeft = e.currentTarget.scrollLeft;
+    setCarouselScrollPosition(scrollLeft);
+  };
+
+  // after clicking on a carousel arrow, adjust the current image index &
+  // increase the auto scroll delay to prevent the carousel
+  // from automatically scrolling immediately after user interaction.
+
+  const handleNextImage = () => {
+    setAutoScrollDelay(AUTO_SCROLL_DELAY_AFTER_USER_INTERACTION_IN_MS);
+    const onLastImage = currentImageIndex === imageObjectArray.length - 1;
+    onLastImage
+      ? setCurrentImageIndex(0)
+      : setCurrentImageIndex(currentImageIndex + 1);
+  };
+
+  const handlePreviousImage = () => {
+    setAutoScrollDelay(AUTO_SCROLL_DELAY_AFTER_USER_INTERACTION_IN_MS);
+    const onFirstImage = currentImageIndex === 0;
+    onFirstImage
+      ? setCurrentImageIndex(imageObjectArray.length - 1)
+      : setCurrentImageIndex(currentImageIndex - 1);
+  };
+
+  // after clicking on a navigation slide image, adjust the current image index &
+  // increase the auto scroll delay to prevent the carousel
+  // from automatically scrolling immediately after user interaction.
+
+  const handleNavigationSlideClick = (imageIndex: number) => {
+    setAutoScrollDelay(AUTO_SCROLL_DELAY_AFTER_USER_INTERACTION_IN_MS);
+    setCurrentImageIndex(imageIndex);
+  };
+
+  // after clicking on a carousel image, open the modal
+
+  const handleCarouselImageClick = (responsiveImage: TResponsiveImage) => {
+    setModalImage(responsiveImage);
+  };
+
+  // after clicking on the opened modal image, close the modal
+
+  const handleModalImageClick = () => {
+    setModalImage(null);
+  };
+
+  // on window width change, set carousel slide & nav slide widths
 
   useEffect(
     function setSlideWidthsOnWindowWidthChange() {
@@ -37,17 +92,51 @@ function Carousel({ imageObject }: TProps) {
     [windowWidth],
   );
 
+  // automatically scroll carousel images on a timer. the timer is reset
+  // whenever the user manually changes the image or opens the modal.
+  // once the user stops interacting with the carousel,
+  // the timer is reset to the default value.
+
   useEffect(
     function autoScrollImagesOnTimer() {
       const autoScroll = setInterval(() => {
-        handleNextImage();
-      }, 2500);
+        setAutoScrollDelay(AUTO_SCROLL_DELAY_DEFAULT_IN_MS);
+        const onLastImage = currentImageIndex === imageObjectArray.length - 1;
+        onLastImage
+          ? setCurrentImageIndex(0)
+          : setCurrentImageIndex(currentImageIndex + 1);
+      }, autoScrollDelay);
       // stop auto scroll when modal is open
       if (modalImage) clearInterval(autoScroll);
       return () => clearInterval(autoScroll);
     },
-    [modalImage, currentImageIndex],
+    // by adding currentImageIndex to the dependency array, we ensure that the
+    // auto scroll timer is reset whenever the user manually changes the image
+    [modalImage, currentImageIndex, imageObjectArray, autoScrollDelay],
   );
+
+  // after the user manually scrolls the carousel, determine the current image index
+  // based on the carousel scroll position. this is done after a debounce to allow the
+  // css scroll animation to complete and to prevent the image index from changing
+  // while the user is still scrolling. once the user stops scrolling & the image index
+  // is updated, the nav slide is scrolled to the center of the nav grid via the
+  // useEffect after this one.
+
+  useEffect(
+    function changeImageIndexAfterCarouselScrollingStops() {
+      const setImageIndexAfterDebounce = setTimeout(() => {
+        const index = Math.round(carouselScrollPosition / carouselSlideWidth);
+        setCurrentImageIndex(index);
+      }, MANUAL_SCROLL_DEBOUNCE_TIME_IN_MS);
+
+      return () => clearTimeout(setImageIndexAfterDebounce);
+    },
+    [carouselScrollPosition, carouselSlideWidth],
+  );
+
+  // regardless of how the image index is changed, scroll to the correct carousel
+  // and navigation slide images. this is done after a debounce to allow the
+  // css scroll animation to complete.
 
   useEffect(
     function scrollToImagesOnIndexChange() {
@@ -58,65 +147,23 @@ function Carousel({ imageObject }: TProps) {
           left: carouselScrollPosition,
           behavior: "smooth",
         });
-
         // scroll current nav slide to center of the nav grid
-        const navGridWidth = navRef.current?.clientWidth ?? 0;
+        const navGridVisibleWidth = navRef.current?.clientWidth ?? 0;
         const navFarLeftScrollPosition = navSlideWidth * currentImageIndex;
         const navCenteredScrollPosition =
-          navFarLeftScrollPosition - (navGridWidth / 2 - navSlideWidth);
+          navFarLeftScrollPosition -
+          navGridVisibleWidth / 2 +
+          navSlideWidth / 2;
         navRef.current?.scrollTo({
           left: navCenteredScrollPosition,
           behavior: "smooth",
         });
-      }, DEBOUNCE_TIME);
+      }, MANUAL_SCROLL_DEBOUNCE_TIME_IN_MS);
 
       return () => clearTimeout(scrollAfterDebounce);
     },
     [currentImageIndex, carouselSlideWidth, navSlideWidth],
   );
-
-  useEffect(
-    function changeImageIndexAfterCarouselScrollingStops() {
-      const setImageIndexAfterDebounce = setTimeout(() => {
-        const index = Math.round(carouselScrollPosition / carouselSlideWidth);
-        setCurrentImageIndex(index);
-      }, DEBOUNCE_TIME);
-
-      return () => clearTimeout(setImageIndexAfterDebounce);
-    },
-    [carouselScrollPosition, carouselSlideWidth],
-  );
-
-  const handleCarouselScroll = (e: React.UIEvent<HTMLDivElement>) => {
-    const scrollLeft = e.currentTarget.scrollLeft;
-    setCarouselScrollPosition(scrollLeft);
-  };
-
-  const handleNextImage = () => {
-    const onLastImage = currentImageIndex === imageObjectArray.length - 1;
-    onLastImage
-      ? setCurrentImageIndex(0)
-      : setCurrentImageIndex(currentImageIndex + 1);
-  };
-
-  const handlePreviousImage = () => {
-    const onFirstImage = currentImageIndex === 0;
-    onFirstImage
-      ? setCurrentImageIndex(imageObjectArray.length - 1)
-      : setCurrentImageIndex(currentImageIndex - 1);
-  };
-
-  const handleCarouselImageClick = (responsiveImage: TResponsiveImage) => {
-    setModalImage(responsiveImage);
-  };
-
-  const handleModalImageClick = () => {
-    setModalImage(null);
-  };
-
-  const handleNavigationSlideClick = (imageIndex: number) => {
-    setCurrentImageIndex(imageIndex);
-  };
 
   return (
     <>
